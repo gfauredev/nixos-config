ae465a8e521c5d065fd205f7b1a0a777aec5c03c
Guilhem Faure <pro@gfaure.eu>
2023-09-14 19:05:56 +0200

    refiled gui.nix in system/misc

--- system/misc.nix
+++ system/misc.nix
@@ -61,7 +61,43 @@
 
       exfat
       ntfs3g
+
+      # Graphical
+      # mesa
+      # libsForQt5.breeze-gtk
+      # libsForQt5.breeze-qt5
+      # libsForQt5.breeze-icons
+      # libsForQt5.qt5.qtwayland
+      # qt6.qtwayland
+      # polkit_gnome
+      # swt
     ];
+    sessionVariables = {
+      # TEST relevance of each
+      # GTK_IM_MODULE = "ibus";
+      # NIXOS_OZONE_WL = "1";
+      # XCURSOR_THEME = "Nordzy-cursors";
+      # WLR_DRM_NO_ATOMIC = "1";
+      # WLR_NO_HARDWARE_CURSORS = "1";
+      # GBM_BACKEND = "nvidia-drm";
+      # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      # LIBVA_DRIVER_NAME = "nvidia";
+      # QT_QPA_PLATFORM = "wayland";
+      # QT_QPA_PLATFORM_PLUGIN_PATH = "/run/current-system/sw/lib";
+
+      # QT_IM_MODULE=xim;
+      # GTK_IM_MODULE=xim;
+      # XMODIFIERS="@im=none";
+
+      # SDL_VIDEODRIVER=wayland;
+
+      # GTK_THEME = "Breeze:dark";
+      # CALIBRE_USE_DARK_PALETTE = "1";
+      # ANKI_WAYLAND = "1";
+
+      # EGL_PLATFORM = "wayland";
+      # MOZ_DISABLE_RDD_SANDBOX = "1";
+    };
   };
 
   security = {
@@ -69,4 +105,53 @@
     # polkit.enable = true; # TODO: test pertinence
     # apparmor.enable = true; # TODO: test pertinence
   };
+
+  # systemd = { # TEST relevance
+  #   user.services.polkit-gnome-authentication-agent-1 = {
+  #     description = "polkit-gnome-authentication-agent-1";
+  #     wantedBy = [ "sway-session.target" ];
+  #     wants = [ "sway-session.target" ];
+  #     after = [ "sway-session.target" ];
+  #     serviceConfig = {
+  #       Type = "simple";
+  #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+  #       Restart = "on-failure";
+  #       RestartSec = 1;
+  #       TimeoutStopSec = 10;
+  #     };
+  #   };
+  # };
+
+  # hardware.opengl = { # TEST relevance
+  #   enable = true;
+  #   driSupport = true;
+  #   driSupport32Bit = true;
+  # };
+
+  # musnix = { # TEST relevance
+  #   enable = true;
+  #   kernel = {
+  #     # realtime = true;
+  #   };
+  # };
+
+  # qt = { # TEST if relevant
+  #   enable = true;
+  #   # platformTheme = "gtk2";
+  #   # style = "gtk2";
+  #   # platformTheme = "qt5ct";
+  #   # style = "adwaita-dark";
+  # };
+
+  # xdg = { # TEST if relevant
+  #   portal = {
+  #     enable = true;
+  #     wlr.enable = true;
+  #     xdgOpenUsePortal = true;
+  #   };
+  #     mime = {
+  #       enable = true;
+  #     };
+  #     autostart.enable = true;
+  # };
 }
