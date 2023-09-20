591d648dc9a7fcfcacea3aff8fdf349d23962a61
Guilhem Fauré <pro@gfaure.eu>
2023-09-20 09:15:06 +0200

    vim dash params

--- neovim/set.lua
+++ neovim/set.lua
@@ -73,13 +73,25 @@ require "dashboard".setup {   -- Dashboard
   theme = "hyper",            --  theme is doom and hyper default is hyper
   disable_move = false,       --  default is false disable move keymap for hyper
   shortcut_type = "letter",   --  shorcut type 'letter' or 'number'
-  change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
+  change_to_vcs_root = true, -- default is false,for open file in hyper mru. it will change to the root of vcs
   config = {
     week_header = {
       enable = true,
     },
     shortcut = {
       { desc = "Feels like we’re gonna edit some text" },
+      {
+        icon = "󰱼 ",
+        desc = "",
+        action = "Telescope find_files",
+        key = "f",
+      },
+      {
+        icon = "󱎸 ",
+        desc = "",
+        action = "Telescope live_grep",
+        key = "g",
+      },
     },
     footer = {},
   },                   --  config used for theme
