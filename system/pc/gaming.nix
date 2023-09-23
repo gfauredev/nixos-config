40b05d91c3ec2bbe456025cf7ff2e2906c1283df
Guilhem Fauré <pro@gfaure.eu>
2023-09-23 15:26:37 +0200

    enable gamemode

--- system/pc/gaming.nix
+++ system/pc/gaming.nix
@@ -8,10 +8,13 @@
     };
   };
 
-  programs.steam = {
-    enable = true;
-    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play TEST
-    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server TEST
+  programs = {
+    steam = {
+      enable = true;
+      # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play TEST
+      # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server TEST
+    };
+    gamemode.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
