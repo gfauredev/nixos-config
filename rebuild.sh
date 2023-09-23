1e7060e6d1b6cf8642cd59372e3068204a80d523
Guilhem Fauré <pro@gfaure.eu>
2023-09-23 02:32:14 +0200

    update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@ if [ "$1" = "os" ] || [ "$1" = "system" ] || [ "$1" = "all" ]; then
   sudo nixos-rebuild --flake . switch
 fi
 
-if [ "$1" != "os" ] || [ "$1" = "system" ]; then
+if [ "$1" != "os" ] || [ "$1" != "system" ]; then
   home-manager --flake .#${USER}@$(hostname) switch
 fi
