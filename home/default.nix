616009ebaf76f222e9d17a2c59b0d22f58f82498
Guilhem Fauré <pro@gfaure.eu>
2023-09-25 14:38:30 +0200

    disable platformtheme

--- home/default.nix
+++ home/default.nix
@@ -13,10 +13,10 @@
     # Theme & Style
     # libsForQt5.qt5ct # TEST if relevant
     # libsForQt5.qtstyleplugin-kvantum # TEST if relevant
-    libsForQt5.qt5.qtwayland # TEST if relevant
+    # libsForQt5.qt5.qtwayland # TEST if relevant
     # qt6Packages.qt6ct # TEST if relevant
     # qt6Packages.qtstyleplugin-kvantum # TEST if relevant
-    qt6.qtwayland # TEST if relevant
+    # qt6.qtwayland # TEST if relevant
     # libsForQt5.systemsettings # TEST if relevant
     # adwaita-qt # TEST if relevant
 
@@ -155,7 +155,7 @@
   qt = {
     enable = true; # FIXME
     # platformTheme = "gtk"; # TEST if relevant
-    platformTheme = "qtct"; # TEST if relevant
+    # platformTheme = "qtct"; # TEST if relevant
     style = {
       # name = "gtk2";
       name = "adwaita-dark";
