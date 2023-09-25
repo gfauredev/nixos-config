b9755191f3696862d3df5ceab9d1558a90cb7fe0
Guilhem Fauré <pro@gfaure.eu>
2023-09-25 14:20:56 +0200

    delete mimeapps.list before upgrade

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,11 @@
 if [ "$1" = "os" ] || [ "$1" = "system" ] || [ "$1" = "all" ]; then
   sudo mount /boot # Use fstab
   sudo nixos-rebuild --flake . switch
-  sudo umount /boot # Use fstab
+  sudo umount /boot # Unmount for security
 fi
 
 if [ "$1" != "os" ] && [ "$1" != "system" ]; then
+  rm -f $XDG_CONFIG_HOME/mimeapps.list # Mitigate Mailspring behavior
   home-manager --flake .#${USER}@$(hostname) switch
 fi
 
