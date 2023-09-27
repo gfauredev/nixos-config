a99e348ff68a1f6130abd11e5448ddea851265a3
Guilhem Fauré <pro@gfaure.eu>
2023-09-27 18:45:56 +0200

    use typst watch in typ

--- home/zsh.nix
+++ home/zsh.nix
@@ -16,8 +16,10 @@
       # TODO make typst-env built into docu dev shell
       typst-env = pkgs.writeShellScriptBin "typ" ''
         TYPST_LIB="$HOME/.local/share/typst-templates"
+
         ln -s $TYPST_LIB lib.typ
-        term "watchexec -w $1 -w $TYPST_LIB typst compile $1; rm -fv lib.typ" .
+        # term "watchexec -w $1 -w $TYPST_LIB typst compile $1; rm -fv lib.typ" .
+        term "typst watch $1; rm -fv lib.typ" .
         pdf="$(echo $1|cut -d"." -f1).pdf"
         echo "Oppening the file $pdf"
         xdg-open $pdf
