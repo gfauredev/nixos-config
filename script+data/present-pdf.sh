fa8e2f723586498673ffc04a802d4eaed497b9bb
Guilhem Fauré <pro@gfaure.eu>
2023-10-01 18:11:11 +0200

    fix present script

--- script+data/present-pdf.sh
+++ script+data/present-pdf.sh
@@ -1,7 +1,4 @@
-present () { # Open with PDF presenter console
-  # [ -n "$2" ] && presentation="$2" || presentation="1" FIXME
-  # [ -n "$3" ] && presenter="$3" || presenter="0" FIXME
-  # pdfpc --presenter-screen=$presenter --presentation-screen=$presentation "$1" & disown
-  pdfpc "$1" & disown
-}
-
+# [ -n "$2" ] && presentation="$2" || presentation="1" FIXME
+# [ -n "$3" ] && presenter="$3" || presenter="0" FIXME
+# pdfpc --presenter-screen=$presenter --presentation-screen=$presentation "$1" & disown
+pdfpc "$1" & disown
