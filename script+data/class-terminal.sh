90b23aced7f0b7c25890f5eaa7c6cccd89c2486d
Guilhem Fauré <pro@gfaure.eu>
2023-10-02 15:59:41 +0200

    fix class terminal

--- script+data/class-terminal.sh
+++ script+data/class-terminal.sh
@@ -1,3 +1,5 @@
 [ -n "$1" ] && WD="--cwd=$1" || WD="--cwd=$PWD"
 
-wezterm start $WD --class $0 "${@:2}" & disown
+filename="$(echo "$0" | grep -o "[^/]*$")"
+
+wezterm start $WD --class $filename "${@:2}" & disown
