0bb420ced148ea18fb141b206378834fec7e2575
Guilhem Fauré <pro@gfaure.eu>
2023-10-02 14:27:23 +0200

    fix typst script

--- script+data/typst-env.sh
+++ script+data/typst-env.sh
@@ -1,6 +1,6 @@
 TYPST_LIB="$HOME/.local/share/typst-templates"
 
-ln -s $TYPST_LIB $(dirname $1)/lib.typ
+[ -h "$(dirname $1)/lib.typ" ] || ln -s $TYPST_LIB $(dirname $1)/lib.typ
 
 # t "watchexec -w $1 -w $TYPST_LIB typst compile $1; rm -fv lib.typ" .
 t . sh -c "typst watch $1; \rm -f $(dirname $1)/lib.typ" 
