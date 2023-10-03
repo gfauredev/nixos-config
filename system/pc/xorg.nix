ba2160fb4f454cfba4c21953af54d227be99988a
Guilhem Fauré <pro@gfaure.eu>
2023-10-03 18:32:28 +0200

    remove xbanish

--- system/pc/xorg.nix
+++ system/pc/xorg.nix
@@ -1,12 +1,10 @@
 { inputs, lib, config, pkgs, ... }: {
   services = {
     xserver = {
-      # exportConfiguration = true; # TEST relevance
       displayManager = {
         startx.enable = true;
         defaultSession = "none+i3";
       };
     };
-    xbanish.enable = true;
   };
 }
