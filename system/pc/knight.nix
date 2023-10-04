b4b2b6f4ef2c44cc69c7bc3262e941e9112f5ae6
Guilhem Fauré <pro@gfaure.eu>
2023-10-04 23:14:01 +0200

    switch to beta nvidia driver

--- system/pc/knight.nix
+++ system/pc/knight.nix
@@ -15,8 +15,8 @@
       powerManagement.enable = false;
       powerManagement.finegrained = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-      # package = config.boot.kernelPackages.nvidiaPackages.beta;
+      # package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
       # forceFullCompositionPipeline = true; TEST relevance
       # nvidiaPersistenced = true; TEST relevance
     };
