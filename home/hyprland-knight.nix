386aeeee4a7085bdda8553d0b9896a0a65fe20e1
Guilhem Fauré <pro@gfaure.eu>
2023-10-07 15:41:00 +0200

    fix knights monitor settings

--- home/hyprland-knight.nix
+++ home/hyprland-knight.nix
@@ -3,19 +3,19 @@
     enableNvidiaPatches = true;
     settings = {
       # See https://wiki.hyprland.org/Configuring/Monitors
-      # knight (ultrawide disktop) monitors
+      # knight (ultrawide desktop) monitors
       monitor = [
-        "eDP-1,2256x1504,0x0,1.4" # knight’s main monitor
+        "DP-1,3440x1440,0x0,1.25" # knight’s main monitor
         ",preferred,auto,1" # Externals monitor
         # ",preferred,auto,1,mirror,eDP-1" # Mirrored external monitors
       ];
 
       # See https://wiki.hyprland.org/Configuring/Workspace-Rules
-      # knight (ultrawide disktop) workspaces
+      # knight (ultrawide desktop) workspaces
       workspace = [
-        "name:web,monitor:eDP-1,default:true"
-        "name:dpp,monitor:DP-1,default:true"
-        "name:hdm,monitor:DP-2,default:true"
+        "name:web,monitor:DP-1,default:true"
+        "name:dpp,monitor:DP-2,default:true"
+        "name:hdm,monitor:HDMI-A-1,default:true"
         "name:sup,monitor:DP-3,default:true"
         "name:ext,monitor:DP-4,default:true"
       ];
