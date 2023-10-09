adee5d0f0900a2695a777d89389ed40fcbf9b8a5
Guilhem Fauré <pro@gfaure.eu>
2023-10-09 16:46:06 +0200

    typ alias: open pdf but don’t open text file

--- home/zsh/typst-env.sh
+++ home/zsh/typst-env.sh
@@ -1,12 +1,15 @@
+# Link the Typst library to use inside Typst file
 TYPST_LIB="$HOME/.local/share/typst-templates"
-
+# TODO enhance this with proper Typst imports
 [ -h "$(dirname $1)/lib.typ" ] || ln -s $TYPST_LIB $(dirname $1)/lib.typ
 
+# Recompile the Typst file at each modification
 # t "watchexec -w $1 -w $TYPST_LIB typst compile $1; rm -fv lib.typ" .
 t . sh -c "typst watch $1; \rm -f $(dirname $1)/lib.typ" 
 
-# pdf="$(echo $1|cut -d"." -f1).pdf"
-# echo "Oppening the file $pdf"
-# xdg-open $pdf
+# Open generated PDF in PDF viewer
+PDF="$(echo $1 | sd "typ" "pdf")"
+xdg-open $pdf
 
-$EDITOR $1
+# Open Typst file in text editor
+# $EDITOR $1
