ae057badbcd205d5075351d51e8e6f412f990406
Guilhem Fauré <pro@gfaure.eu>
2023-10-10 18:59:19 +0200

    add whatsapp

--- home/social.nix
+++ home/social.nix
@@ -1,11 +1,13 @@
 { inputs, lib, config, pkgs, ... }: {
   home.packages = with pkgs; [
-    himalaya # CLI mail client # TODO add via home manager module
+    himalaya # CLI mail client # Configure via home manager module
     element-desktop # Matrix messaging
     signal-desktop # Secure messaging
     # element-web # Matrix messaging
     # mailspring # Email client WARNING security issue
+    # UNFREE #
     discord # PROPRIETARY messaging and general communication
+    whatsapp-for-linux # PROPRIETARY messaging and general communication
     # discord-canary # PROPRIETARY messaging and general communication
     # teams-for-linux # PROPRIETARY services messaging and work
     # zoom         # PROPRIETARY messaging and work
