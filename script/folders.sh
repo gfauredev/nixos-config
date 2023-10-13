180cd0ab2f2f688c34beebbd347977c1cbb18a89
Guilhem Fauré <pro@gfaure.eu>
2023-10-14 00:36:21 +0200

    update folders.sh

--- script/folders.sh
+++ script/folders.sh
@@ -7,18 +7,27 @@
 # - Names are always singular, never plural
 # - Names are kept short, while still be totally clear and non-ambiguous on their nature
 
+echo "== Org == Textual organisation files =="
+if [ ! -d $HOME/org ]; then
+  echo "\nCreating org directory …"
+  mkdir -m 700 -v $HOME/org/
+fi
+echo "#include stignore" > $HOME/org/.stignore
+echo "Organization" > $HOME/org/.ventoyignore
+echo
+
 echo "== Project == Projects (containing sub-projects) =="
 echo "  Tasks required to progress toward goal or next milestone"
 echo "  Knowledge & data that are useful for the project"
 if [ ! -d $HOME/project ]; then
   echo "\nCreating project directory and subdirectories …"
   mkdir -m 700 -v $HOME/project/
-  echo "#include stignore" > $HOME/project/.stignore
-  echo "Project" > $HOME/project/.ventoyignore
   echo "\nCreating large project directory to avoid on devices with fewer storage space …"
   mkdir -m 700 -v $HOME/project.large/
-  echo "#include stignore" > $HOME/project.large/.stignore
 fi
+echo "#include stignore" > $HOME/project/.stignore
+echo "Project" > $HOME/project/.ventoyignore
+echo "#include stignore" > $HOME/project.large/.stignore
 echo
 
 echo "== Life == Broad life areas =="
@@ -27,8 +36,6 @@ echo "  Tasks that needs to be done regularly and not ending projects"
 if [ ! -d $HOME/life ]; then
   echo "\nCreating life directory and subdirectories …"
   mkdir -m 700 -v $HOME/life/
-  echo "#include stignore" > $HOME/life/.stignore
-  echo "Life" > $HOME/data/.ventoyignore
   mkdir -m 700 -v $HOME/life/health/
   mkdir -m 700 -v $HOME/life/identity/
   mkdir -m 700 -v $HOME/life/relation/
@@ -36,8 +43,10 @@ if [ ! -d $HOME/life ]; then
   mkdir -m 700 -v $HOME/life/degree+certification/
   echo "\nCreating large life directory to avoid on devices with fewer storage space …"
   mkdir -m 700 -v $HOME/life.large/
-  echo "#include stignore" > $HOME/life.large/.stignore
 fi
+echo "#include stignore" > $HOME/life/.stignore
+echo "Life" > $HOME/data/.ventoyignore
+echo "#include stignore" > $HOME/life.large/.stignore
 echo
 
 echo "== Data == Unsorted or type-sorted data =="
@@ -46,8 +55,6 @@ echo "  Interesting knowledge that might be worth or fun to learn"
 if [ ! -d $HOME/data ]; then
   echo "\nCreating data directory and subdirectories …"
   mkdir -m 700 -v $HOME/data/
-  echo "#include stignore" > $HOME/data/.stignore
-  echo "Data" > $HOME/data/.ventoyignore
   mkdir -m 700 -v $HOME/data/document/
   mkdir -m 700 -v $HOME/data/document.text/
   mkdir -m 700 -v $HOME/data/audio/
@@ -60,7 +67,6 @@ if [ ! -d $HOME/data ]; then
   mkdir -m 700 -v $HOME/data/video/movie.entertain/
   echo "\nCreating large data directory to avoid on devices with fewer storage space …"
   mkdir -m 700 -v $HOME/data.large/
-  echo "#include stignore" > $HOME/data.large/.stignore
   mkdir -m 700 -v $HOME/data.large/code/
   mkdir -m 700 -v $HOME/data.large/audio/
   mkdir -m 700 -v $HOME/data.large/audio/record/
@@ -71,6 +77,9 @@ if [ ! -d $HOME/data ]; then
   mkdir -m 700 -v $HOME/data.large/video/screenrecord/
   mkdir -m 700 -v $HOME/data.large/video/movie.entertain/
 fi
+echo "#include stignore" > $HOME/data/.stignore
+echo "Data" > $HOME/data/.ventoyignore
+echo "#include stignore" > $HOME/data.large/.stignore
 echo
 
 echo "== Archive == Sorting kept as-is =="
@@ -79,9 +88,10 @@ echo "  Definitely used or learned knowledge"
 if [ ! -d $HOME/archive ]; then
   echo "\nCreating archive directory and subdirectories …"
   mkdir -m 700 -v $HOME/archive/
-  echo "#include stignore" > $HOME/archive/.stignore
   mkdir -m 700 -v $HOME/archive/project/
   mkdir -m 700 -v $HOME/archive/life/
   mkdir -m 700 -v $HOME/archive/data/
 fi
+echo "#include stignore" > $HOME/archive/.stignore
+echo "Archive" > $HOME/archive/.ventoyignore
 echo
