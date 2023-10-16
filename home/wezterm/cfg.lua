fcb5ab051a4356b57a447fb34762336fd11dca51
Guilhem Fauré <pro@gfaure.eu>
2023-10-16 18:09:29 +0200

    reenable, no effect

--- home/wezterm/cfg.lua
+++ home/wezterm/cfg.lua
@@ -2,7 +2,7 @@ local wezterm = require "wezterm"
 -- General options
 cfg = {
   enable_wayland = true,
-  frontt_end = "Software", -- Disable hardware acceleration for nvidia GPU
+  -- front_end = "Software", -- Disable hardware acceleration for nvidia GPU
   font = wezterm.font "FiraCode Nerd Font",
   enable_tab_bar = false,
   window_background_opacity = 0.8,
