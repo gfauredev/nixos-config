f0119807adacb930734640061753ffa831138cba
Guilhem Fauré <pro@gfaure.eu>
2023-10-19 17:42:27 +0200

    reenable closed nvidia driver

--- system/pc/knight.nix
+++ system/pc/knight.nix
@@ -21,17 +21,17 @@
     };
   };
 
-  # specialisation.closed-nvidia.configuration = {
-  #   hardware = {
-  #     nvidia = {
-  #       open = lib.mkForce false;
-  #       modesetting.enable = true;
-  #       powerManagement.enable = false;
-  #       powerManagement.finegrained = false;
-  #       nvidiaSettings = true;
-  #     };
-  #   };
-  # };
+  specialisation.closed-nvidia.configuration = {
+    hardware = {
+      nvidia = {
+        open = lib.mkForce false;
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        nvidiaSettings = true;
+      };
+    };
+  };
 
   boot = {
     loader = {
