9f63eb5ac2c12bca89f441301987696f0719face
Guilhem Fauré <pro@gfaure.eu>
2023-10-20 16:38:38 +0200

    add protonup-ng

--- home/gaming.nix
+++ home/gaming.nix
@@ -5,5 +5,6 @@
     legendary-gl # Epic games launcher alternative
     # moonlight-qt # Game streaming client # TEST if better than rustdesk
     mangohud # A practical HUD on top of games
+    protonup-ng # Manage installation of proton-ge
   ];
 }
