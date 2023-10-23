a3360167c4d7e8335a05540d6953b22696747510
Guilhem Fauré <pro@gfaure.eu>
2023-10-24 00:16:07 +0200

    better term launching in hyprland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -56,123 +56,127 @@
 
       # See https://wiki.hyprland.org/Configuring/Keywords
       "$mod" = "SUPER";
-      bind = [
-        # Move focus
-        "$mod, c, movefocus, l" # Move left
-        "$mod, t, movefocus, d" # Move down
-        "$mod, s, movefocus, u" # Move up
-        "$mod, r, movefocus, r" # Move right
-        # Move window
-        "$mod SHIFT, c, movewindoworgroup, l" # Move left
-        "$mod SHIFT, t, movewindoworgroup, d" # Move down
-        "$mod SHIFT, s, movewindoworgroup, u" # Move up
-        "$mod SHIFT, r, movewindoworgroup, r" # Move right
-        "$mod CONTROL SHIFT, c, swapwindow, l" # Move left
-        "$mod CONTROL SHIFT, t, swapwindow, d" # Move down
-        "$mod CONTROL SHIFT, s, swapwindow, u" # Move up
-        "$mod CONTROL SHIFT, r, swapwindow, r" # Move right
-        "$mod, g, exec, hyprctl -j activewindow | jq -e '.grouped[0,1]' && hyprctl dispatch changegroupactive f || hyprctl dispatch togglegroup" # Smart group control
-        "$mod CONTROL, g, togglegroup," # Toggle group
-        "$mod SHIFT, g, changegroupactive, f" # Toggle focus in group
-        "$mod CONTROL SHIFT, g, changegroupactive, b" # Toggle focus in group
-        # Workspaces (Left)
-        "$mod, b, workspace, name:web" # Browsing workspace
-        "$mod, b, exec, hyprctl clients | grep -i 'class: brave-browser' || brave" # Auto open browser if not running
-        "$mod SHIFT, b, movetoworkspace, name:web" # Web browser
-        "$mod, a, workspace, name:aud" # Audio workspace
-        "$mod, a, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"aud\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
-        "$mod SHIFT, a, movetoworkspace, name:aud" # Audio workspace
-        "$mod, o, workspace, name:opn" # Open (a file)
-        "$mod, o, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"opn\"' | grep true || wezterm start zsh -ic 'br;zsh'" # Start a term with explorer
-        "$mod, i, workspace, name:top" # Informations / monItorIng
-        "$mod, i, exec, hyprctl clients | grep -i 'class: monitoring' || wezterm start --class monitoring btm" # Auto open bottom if not running
-        # /!\ Cannot move to Monitoring worspace
-        # Additional workspaces (Left)
-        "$mod, u, workspace, name:sup" # Sup / Supplementary workspace
-        "$mod SHIFT, u, movetoworkspace, name:sup" # Sup / Supplementary
-        "$mod, e, workspace, name:etc" # Etc (et cetera) workspace
-        "$mod, e, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"etc\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
-        "$mod SHIFT, e, movetoworkspace, name:etc" # Etc (et cetera)
-        "$mod, x, workspace, name:ext" # Ext / Extra workspace
-        "$mod, x, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"ext\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
-        "$mod SHIFT, x, movetoworkspace, name:ext" # Ext / Extra
-        # Workspaces (Right)
-        "$mod, l, workspace, name:cli" # cLi / terminaL workspace
-        "$mod, l, exec, hyprctl clients -j | jq '.[]|([.class,.workspace.name] == [\"org.wezfurlong.wezterm\",\"cli\"])' | grep true || wezterm start" # Auto open CLI if not running
-        "$mod SHIFT, l, movetoworkspace, name:cli" # cLi / terminaL
-        "$mod, n, workspace, name:not" # Notetaking workspace
-        "$mod, n, exec, hyprctl clients | grep -i 'class: note' || wezterm start --cwd ~/note --class note $EDITOR" # Auto open text editor
-        "$mod SHIFT, n, movetoworkspace, name:not" # Notetaking
-        "$mod, m, workspace, name:msg" # Messaging workspace
-        "$mod, m, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"msg\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
-        "$mod SHIFT, m, movetoworkspace, name:msg" # Messaging
-        # Additional monitor workspaces (Right)
-        "$mod, d, workspace, name:dpp" # DisplayPort workspace
-        "$mod SHIFT, d, movetoworkspace, name:dpp" # DisplayPort
-        "$mod, h, workspace, name:hdm" # HDMI workspace
-        "$mod SHIFT, h, movetoworkspace, name:hdm" # HDMI
-        # Workspaces (Special)
-        ", XF86AudioMedia, workspace, name:med" # Media ws
-        ", XF86AudioMedia, exec, hyprctl clients | grep -i 'title: spotify' || spotify" # Auto open main media player
-        "SHIFT, XF86AudioMedia, exec, menu ~ pulsemixer" # Open audio mixer
-        "CONTROL, XF86AudioMedia, workspace, name:med" # Media ws
-        "CONTROL, XF86AudioMedia, exec, hyprctl clients | grep -i 'class: org.pipewire.Helvum' || helvum" # Auto open audio router
-        "CONTROL SHIFT, XF86AudioMedia, workspace, name:med" # Media ws
-        "CONTROL SHIFT, XF86AudioMedia, exec, hyprctl clients | grep -i 'title: Easy Effects' || easyeffects" # Auto open audio tweaker
-        ", XF86Tools, workspace, name:med" # Media ws
-        ", XF86Tools, exec, hyprctl clients | grep -i 'title: spotify' || spotify" # Auto open main media player
-        "SHIFT, XF86Tools, exec, menu ~ pulsemixer" # Open audio mixer
-        "CONTROL, XF86Tools, workspace, name:med" # Media ws
-        "CONTROL, XF86Tools, exec, hyprctl clients | grep -i 'class: org.pipewire.Helvum' || helvum" # Auto open audio router
-        "CONTROL SHIFT, XF86Tools, workspace, name:med" # Media ws
-        "CONTROL SHIFT, XF86Tools, exec, hyprctl clients | grep -i 'title: Easy Effects' || easyeffects" # Auto open audio tweaker
-        # /!\ Cannot move to Media worspace
-        # Terminal # TODO test multiplexing, features of wezterm
-        "$mod, RETURN, exec, ${pkgs.wezterm}/bin/wezterm start"
-        # Launch
-        "$mod, SPACE, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji"
-        "$mod CONTROL, SPACE, exec, rofi -show calc"
-        "$mod SHIFT, SPACE, exec, rofi -show-icons -show top"
-        # Launch with special media keys TODO use them better
-        ", Menu, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Menu special key
-        "SUPER, j, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # F9 on Framework Laptop
-        ", XF86Mail, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Mail media key
-        ", XF86HomePage, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Home media key
-        ", XF86Calculator, exec, rofi -show calc" # Calculator media key
-        ", XF86Search, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Search media key
-        # Manage windows
-        "$mod, f, togglefloating," # Float window
-        "$mod, w, fullscreen," # Fullscreen window
-        "$mod, q, killactive," # Close window
-        # System control
-        "$mod CONTROL SHIFT, q, exit," # Close wayland session
-        "$mod, comma, exec, swaylock -f -i $HOME/.wallpaper/desert.jpg"
-        "$mod SHIFT, comma, exec, systemctl suspend"
-        # Web
-        "$mod CONTROL, b, exec, nyxt" # Alternative browser
-        # "$mod SHIFT, b, exec, chromium" # Alternative browser
-        "$mod CONTROL SHIFT, b, exec, firefox" # Alternative browser
-        # Audio
-        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        "SHIFT, XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        "SHIFT, XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        # Media
-        ", XF86AudioPlay, exec, playerctl play-pause"
-        "SHIFT, XF86AudioPlay, exec, playerctl play-pause -p spotify"
-        ", XF86AudioPause, exec, playerctl play-pause"
-        "SHIFT, XF86AudioPause, exec, playerctl play-pause -p spotify"
+      bind =
+        let
+          term = "${pkgs.wezterm}/bin/wezterm start --always-new-process";
+        in
+        [
+          # Move focus
+          "$mod, c, movefocus, l" # Move left
+          "$mod, t, movefocus, d" # Move down
+          "$mod, s, movefocus, u" # Move up
+          "$mod, r, movefocus, r" # Move right
+          # Move window
+          "$mod SHIFT, c, movewindoworgroup, l" # Move left
+          "$mod SHIFT, t, movewindoworgroup, d" # Move down
+          "$mod SHIFT, s, movewindoworgroup, u" # Move up
+          "$mod SHIFT, r, movewindoworgroup, r" # Move right
+          "$mod CONTROL SHIFT, c, swapwindow, l" # Move left
+          "$mod CONTROL SHIFT, t, swapwindow, d" # Move down
+          "$mod CONTROL SHIFT, s, swapwindow, u" # Move up
+          "$mod CONTROL SHIFT, r, swapwindow, r" # Move right
+          "$mod, g, exec, hyprctl -j activewindow | jq -e '.grouped[0,1]' && hyprctl dispatch changegroupactive f || hyprctl dispatch togglegroup" # Smart group control
+          "$mod CONTROL, g, togglegroup," # Toggle group
+          "$mod SHIFT, g, changegroupactive, f" # Toggle focus in group
+          "$mod CONTROL SHIFT, g, changegroupactive, b" # Toggle focus in group
+          # Workspaces (Left)
+          "$mod, b, workspace, name:web" # Browsing workspace
+          "$mod, b, exec, hyprctl clients | grep -i 'class: brave-browser' || brave" # Auto open browser if not running
+          "$mod SHIFT, b, movetoworkspace, name:web" # Web browser
+          "$mod, a, workspace, name:aud" # Audio workspace
+          "$mod, a, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"aud\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
+          "$mod SHIFT, a, movetoworkspace, name:aud" # Audio workspace
+          "$mod, o, workspace, name:opn" # Open (a file)
+          "$mod, o, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"opn\"' | grep true || ${term} zsh -ic 'br;zsh'" # Start a term with explorer
+          "$mod, i, workspace, name:top" # Informations / monItorIng
+          "$mod, i, exec, hyprctl clients | grep -i 'class: monitoring' || ${term} --class monitoring btm" # Auto open bottom if not running
+          # /!\ Cannot move to Monitoring worspace
+          # Additional workspaces (Left)
+          "$mod, u, workspace, name:sup" # Sup / Supplementary workspace
+          "$mod SHIFT, u, movetoworkspace, name:sup" # Sup / Supplementary
+          "$mod, e, workspace, name:etc" # Etc (et cetera) workspace
+          "$mod, e, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"etc\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
+          "$mod SHIFT, e, movetoworkspace, name:etc" # Etc (et cetera)
+          "$mod, x, workspace, name:ext" # Ext / Extra workspace
+          "$mod, x, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"ext\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
+          "$mod SHIFT, x, movetoworkspace, name:ext" # Ext / Extra
+          # Workspaces (Right)
+          "$mod, l, workspace, name:cli" # cLi / terminaL workspace
+          "$mod, l, exec, hyprctl clients -j | jq '.[]|([.class,.workspace.name] == [\"org.wezfurlong.wezterm\",\"cli\"])' | grep true || ${term}" # Auto open CLI if not running
+          "$mod SHIFT, l, movetoworkspace, name:cli" # cLi / terminaL
+          "$mod, n, workspace, name:not" # Notetaking workspace
+          "$mod, n, exec, hyprctl clients | grep -i 'class: note' || ${term} --cwd ~/note --class note $EDITOR" # Auto open text editor
+          "$mod SHIFT, n, movetoworkspace, name:not" # Notetaking
+          "$mod, m, workspace, name:msg" # Messaging workspace
+          "$mod, m, exec, hyprctl clients -j | jq '.[]|.workspace.name == \"msg\"' | grep true || rofi -show-icons -show drun" # Auto open laucher
+          "$mod SHIFT, m, movetoworkspace, name:msg" # Messaging
+          # Additional monitor workspaces (Right)
+          "$mod, d, workspace, name:dpp" # DisplayPort workspace
+          "$mod SHIFT, d, movetoworkspace, name:dpp" # DisplayPort
+          "$mod, h, workspace, name:hdm" # HDMI workspace
+          "$mod SHIFT, h, movetoworkspace, name:hdm" # HDMI
+          # Workspaces (Special)
+          ", XF86AudioMedia, workspace, name:med" # Media ws
+          ", XF86AudioMedia, exec, hyprctl clients | grep -i 'title: spotify' || spotify" # Auto open main media player
+          "SHIFT, XF86AudioMedia, exec, menu ~ pulsemixer" # Open audio mixer
+          "CONTROL, XF86AudioMedia, workspace, name:med" # Media ws
+          "CONTROL, XF86AudioMedia, exec, hyprctl clients | grep -i 'class: org.pipewire.Helvum' || helvum" # Auto open audio router
+          "CONTROL SHIFT, XF86AudioMedia, workspace, name:med" # Media ws
+          "CONTROL SHIFT, XF86AudioMedia, exec, hyprctl clients | grep -i 'title: Easy Effects' || easyeffects" # Auto open audio tweaker
+          ", XF86Tools, workspace, name:med" # Media ws
+          ", XF86Tools, exec, hyprctl clients | grep -i 'title: spotify' || spotify" # Auto open main media player
+          "SHIFT, XF86Tools, exec, menu ~ pulsemixer" # Open audio mixer
+          "CONTROL, XF86Tools, workspace, name:med" # Media ws
+          "CONTROL, XF86Tools, exec, hyprctl clients | grep -i 'class: org.pipewire.Helvum' || helvum" # Auto open audio router
+          "CONTROL SHIFT, XF86Tools, workspace, name:med" # Media ws
+          "CONTROL SHIFT, XF86Tools, exec, hyprctl clients | grep -i 'title: Easy Effects' || easyeffects" # Auto open audio tweaker
+          # /!\ Cannot move to Media worspace
+          # Terminal # TODO test multiplexing, features of wezterm
+          "$mod, RETURN, exec, ${term}"
+          # Launch
+          "$mod, SPACE, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji"
+          "$mod CONTROL, SPACE, exec, rofi -show calc"
+          "$mod SHIFT, SPACE, exec, rofi -show-icons -show top"
+          # Launch with special media keys TODO use them better
+          ", Menu, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Menu special key
+          "SUPER, j, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # F9 on Framework Laptop
+          ", XF86Mail, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Mail media key
+          ", XF86HomePage, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Home media key
+          ", XF86Calculator, exec, rofi -show calc" # Calculator media key
+          ", XF86Search, exec, rofi -show-icons -show combi -combi-modes window,drun,ssh,emoji" # Search media key
+          # Manage windows
+          "$mod, f, togglefloating," # Float window
+          "$mod, w, fullscreen," # Fullscreen window
+          "$mod, q, killactive," # Close window
+          # System control
+          "$mod CONTROL SHIFT, q, exit," # Close wayland session
+          "$mod, comma, exec, swaylock -f -i $HOME/.wallpaper/desert.jpg"
+          "$mod SHIFT, comma, exec, systemctl suspend"
+          # Web
+          "$mod CONTROL, b, exec, nyxt" # Alternative browser
+          # "$mod SHIFT, b, exec, chromium" # Alternative browser
+          "$mod CONTROL SHIFT, b, exec, firefox" # Alternative browser
+          # Audio
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          "SHIFT, XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          "SHIFT, XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          # Media
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          "SHIFT, XF86AudioPlay, exec, playerctl play-pause -p spotify"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          "SHIFT, XF86AudioPause, exec, playerctl play-pause -p spotify"
 
-        ", XF86AudioNext, exec, playerctl next"
-        "SHIFT, XF86AudioNext, exec, playerctl next -p spotify"
-        ", XF86AudioPrev, exec, playerctl previous"
-        "SHIFT, XF86AudioPrev, exec, playerctl previous -p spotify"
-        # Misc
-        ", Print, exec, grim -g \"$(slurp)\" $HOME/data/image/screenshot/$(date +'%Y-%m-%d_%Hh%Mm%Ss.png')"
-        "SHIFT, Print, exec, grim $HOME/data/image/screenshot/$(date +'%Y-%m-%d_%Hh%Mm%Ss.png')"
-        "$mod, p, exec, hyprpicker --autocopy"
-        ", XF86RFKill, exec, rfkill toggle all"
-      ];
+          ", XF86AudioNext, exec, playerctl next"
+          "SHIFT, XF86AudioNext, exec, playerctl next -p spotify"
+          ", XF86AudioPrev, exec, playerctl previous"
+          "SHIFT, XF86AudioPrev, exec, playerctl previous -p spotify"
+          # Misc
+          ", Print, exec, grim -g \"$(slurp)\" $HOME/data/image/screenshot/$(date +'%Y-%m-%d_%Hh%Mm%Ss.png')"
+          "SHIFT, Print, exec, grim $HOME/data/image/screenshot/$(date +'%Y-%m-%d_%Hh%Mm%Ss.png')"
+          "$mod, p, exec, hyprpicker --autocopy"
+          ", XF86RFKill, exec, rfkill toggle all"
+        ];
       binde = [
         # Move windows
         "$mod SHIFT, c, moveactive, -10 0" # Move floating left
