8f3ec4614af4abbe598779526799ff8d390d892d
Guilhem Fauré <pro@gfaure.eu>
2023-11-07 19:49:28 +0100

    add a lot of video editors to test which works best

--- home/video.nix
+++ home/video.nix
@@ -4,12 +4,12 @@
     # TEST which is better
     blender # 3D, animation & video editor
     # losslesscut-bin # Basic lossless video edditing using ffmpeg
-    # libsForQt5.kdenlive # video editor
+    libsForQt5.kdenlive # video editor
     shotcut # video editor
     # davinci-resolve # PROPRIETARY video editor
     # glaxnimate # video editing library
     # flowblade # non linear video editor
-    # olive-editor # non linear video editor
+    olive-editor # non linear video editor
     # natron # non linear video editor
 
     # obs-studio
