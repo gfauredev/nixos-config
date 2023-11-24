e10539bd8d97176b80216c68b87d351cd7ab1110
Guilhem Fauré <pro@gfaure.eu>
2023-11-24 09:25:59 +0100

    switch to ccls

--- home/neovim/lsp.lua
+++ home/neovim/lsp.lua
@@ -17,8 +17,8 @@ lsp.setup_servers({
   -- Low level
   "rust_analyzer",
   "arduino_language_server",
-  -- "ccls",
-  "clangd",
+  "ccls",
+  -- "clangd",
   -- Misc
   -- "typst_lsp",
   -- "ltex",
