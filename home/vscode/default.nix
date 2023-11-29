6e725e2584d6c2af4b88190248e143e97b16a798
Guilhem Fauré <pro@gfaure.eu>
2023-11-29 11:30:17 +0100

    add extensions vscode

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -3,12 +3,24 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    # package = pkgs.vscode-with-extensions;
     mutableExtensionsDir = false;
     extensions = with pkgs.vscode-extensions; [
+      # Quality of life
       vscodevim.vim
+      # Nix
+      jnoortheen.nix-ide
+      # Lua
+      sumneko.lua
+      # Python
       # ms-python.pyright
       ms-python.vscode-pylance
       ms-python.python
+      # Rust
+      rust-lang.rust-analyzer
+      techtheawesome.rust-yew
+      # C / C++
+      llvm-vs-code-extensions.vscode-clangd
     ];
     # userSettings = {
     #   # JSON
