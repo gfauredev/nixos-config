5c97ae90a9d8bc187128caea72a6b86241fcdc29
Guilhem Fauré <pro@gfaure.eu>
2023-12-13 08:50:42 +0100

    enable virt-manager

--- home/virtualization.nix
+++ home/virtualization.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     vagrant # VM auto provisionner
-    # virt-manager # GUI frontend to libvirtd
+    virt-manager # GUI frontend to libvirtd
     # looking-glass-client
-    # Containers
     # kubernetes # Container orchestrator
   ];
 }
