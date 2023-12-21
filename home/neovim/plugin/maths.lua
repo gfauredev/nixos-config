43e2bc9fd89acafb1c749b4d074394e164c9f473
Guilhem Fauré <pro@gfaure.eu>
2023-12-21 12:14:46 +0100

    disable virt nabla

--- home/neovim/plugin/maths.lua
+++ home/neovim/plugin/maths.lua
@@ -2,6 +2,6 @@ local mapopt = { noremap = true, silent = true }
 local map = vim.keymap.set
 local nabla = require "nabla"
 
-nabla.enable_virt()
+-- nabla.enable_virt()
 
 map("", "<leader>p", nabla.popup, mapopt)
