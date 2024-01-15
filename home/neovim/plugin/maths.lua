1ab04e926c5192388875f661efd9a88572939935
Guilhem Fauré <pro@gfaure.eu>
2024-01-15 10:40:33 +0100

    change nabla command for toggle virt

--- home/neovim/plugin/maths.lua
+++ home/neovim/plugin/maths.lua
@@ -2,5 +2,6 @@ local mapopt = { noremap = true, silent = true }
 local map = vim.keymap.set
 local nabla = require "nabla"
 
-map("", "<leader>m", nabla.enable_virt, mapopt)
-map("", "<leader>p", nabla.popup, mapopt)
+map("", "<leader>m", nabla.toggle_virt, mapopt)
+-- map("", "<leader>m", nabla.enable_virt, mapopt)
+-- map("", "<leader>p", nabla.popup, mapopt)
