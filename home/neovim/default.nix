7afbf7f946ec2e95c07ee5337ff7f0cbefba53cf
Guilhem Fauré <pro@gfaure.eu>
2024-01-15 10:22:11 +0100

    add latex treesitter

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     tree-sitter-grammars.tree-sitter-typst
+    # tree-sitter-grammars.tree-sitter-latex
   ];
 
   # nixpkgs.overlays = [
--- home/neovim/plugin/maths.lua
+++ home/neovim/plugin/maths.lua
@@ -2,6 +2,5 @@ local mapopt = { noremap = true, silent = true }
 local map = vim.keymap.set
 local nabla = require "nabla"
 
--- nabla.enable_virt()
-
+map("", "<leader>m", nabla.enable_virt, mapopt)
 map("", "<leader>p", nabla.popup, mapopt)
