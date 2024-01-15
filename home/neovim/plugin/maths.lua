688d1df04f6e0ffc84e9879772727bcbf97977dc
Guilhem Fauré <pro@gfaure.eu>
2024-01-15 10:47:33 +0100

    add autoreload nabla

--- home/neovim/plugin/maths.lua
+++ home/neovim/plugin/maths.lua
@@ -2,6 +2,10 @@ local mapopt = { noremap = true, silent = true }
 local map = vim.keymap.set
 local nabla = require "nabla"
 
-map("", "<leader>m", nabla.toggle_virt, mapopt)
+local function toggle_virt()
+  nabla.toggle_virt({ autogen = true })
+end
+
+map("", "<leader>m", toggle_virt, mapopt)
 -- map("", "<leader>m", nabla.enable_virt, mapopt)
 -- map("", "<leader>p", nabla.popup, mapopt)
