95499d0242c4cdacb9735350082d5501159d2402
Guilhem Fauré <pro@gfaure.eu>
2024-01-31 16:46:06 +0100

    improve installer config

--- system/installer.nix
+++ system/installer.nix
@@ -1,6 +1,14 @@
-{ inputs, lib, config, pkgs, ... }: {
+{ lib, pkgs, ... }: {
   nixpkgs.config.allowUnfree = true;
   # hardware.enableAllFirmware = true;
+
+  boot = {
+    # Enable SysRq keys (reboot/off:128, kill:64, sync:16, kbdControl: 4)
+    kernel.sysctl = { "kernel.sysrq" = 212; };
+    # kernelPackages = pkgs.linuxPackages; # Stable Linux kernel
+    kernelPackages = pkgs.linuxPackages_latest; # Latest Linux kernel
+    supportedFilesystems = [ "bcachefs" ]; # Add support for bcachefs
+  };
 
   networking.wireless.enable = false;
 
@@ -37,14 +45,15 @@
   #   extraGroups = [ "wheel" ];
   # };
 
-  # services = {
-  #   openssh = {
-  #     enable = true; # Enable the OpenSSH daemon
-  #     settings = {
-  #       PermitRootLogin = "yes"; # We want to easily configure headlessly
-  #       PasswordAuthentication = true;
-  #     };
-  #   };
-  #   nfs.server.enable = lib.mkDefault true;
-  # };
+  services = {
+    openssh = {
+      enable = true; # Enable the OpenSSH daemon
+      settings = {
+        PasswordAuthentication = true; # Prevent the need to setup a key
+        PermitRootLogin = "yes"; # Easily login as root via SSH
+        LogLevel = "VERBOSE"; # So that fail2ban can observe failed logins
+      };
+    };
+    # nfs.server.enable = lib.mkDefault true;
+  };
 }
