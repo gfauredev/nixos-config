8366e7f8dbba7add1b3fd9d5122acb039bec0dcf
Guilhem Fauré <pro@gfaure.eu>
2024-01-31 10:19:12 +0100

    disable some retrogaming cores

--- home/retrogaming.nix
+++ home/retrogaming.nix
@@ -2,13 +2,13 @@
   home.packages = with pkgs; [
     (retroarch.override {
       cores = with libretro; [
-        beetle-gba # Gamebox Advance
-        snes9x # Super Nintendo
-        genesis-plus-gx # Sega Genesis
         fbneo # Arcade machines
-        mame # Arcade machines
-        mame2003-plus # Arcade machines
-        mame2010 # Arcade machines
+        # mame # Arcade machines
+        # mame2003-plus # Arcade machines
+        # mame2010 # Arcade machines
+        # beetle-gba # Gamebox Advance
+        # snes9x # Super Nintendo
+        # genesis-plus-gx # Sega Genesis
       ];
     })
   ];
