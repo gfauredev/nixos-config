266593aabbee5654809134127fc64b433f9c412f
Guilhem Fauré <pro@gfaure.eu>
2024-01-31 09:51:46 +0100

    broot in not ws opening

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -112,7 +112,8 @@
         "$mod, l, exec, hyprctl clients -j | jq -e 'any(.[]; .workspace.name == \"cli\" and (.class | test(\"${term.name}\";\"i\")))' || ${term.cmd}" # Auto open CLI if not running
         "$mod SHIFT, l, movetoworkspace, name:cli" # cLi / terminaL
         "$mod, n, workspace, name:not" # Notetaking workspace
-        "$mod, n, exec, hyprctl clients | grep -i 'class: note' || ${term.note} $EDITOR" # Auto open text editor
+        "$mod, n, exec, hyprctl clients | grep -i 'class: note' || ${term.note} zsh -ic 'br;zsh'" # Start a term with explorer
+        # "$mod, n, exec, hyprctl clients | grep -i 'class: note' || ${term.note} $EDITOR" # Auto open text editor
         "$mod SHIFT, n, movetoworkspace, name:not" # Notetaking
         "$mod, m, workspace, name:msg" # Messaging workspace
         "$mod, m, exec, hyprctl clients -j | jq -e 'any(.[]; .workspace.name == \"msg\")' || rofi -show-icons -show drun" # Auto open laucher
