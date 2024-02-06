70ce64fb19abc8722bb7d879e04ec579ff0c57c4
Guilhem Fauré <pro@gfaure.eu>
2024-02-06 23:34:37 +0100

    add cq-editor

--- home/model.nix
+++ home/model.nix
@@ -2,11 +2,11 @@
   home.packages = with pkgs; [
     # blender # Most popular 3D, animation & video editor
 
-    # fritzing # Elecronics design
     kicad # Elecronics design
+    # fritzing # Elecronics design
 
+    cq-editor # GUI for Python library CadQuery
     # openscad # Parametric, programmatic (code only) 3D CAD
-    # Use Python library CadQuery instead
 
     freecad # Popular parametric 3D CAD
     # solvespace # Simple parametric 3D CAD
