1f74295bf9b71c83c2d13a9a0999441819ff1193
Guilhem Fauré <pro@gfaure.eu>
2024-02-12 16:35:38 +0100

    fix terahtml filetype

--- home/neovim/filetype.lua
+++ home/neovim/filetype.lua
@@ -7,7 +7,7 @@ vim.filetype.add({ extension = { typ = "typst" } }) -- Add typst filetype
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.html" },
   callback = function()
-    if vim.fn.search("{{.\\{-}}}") ~= 0 and vim.fn.search("{%.\\{-}%}") == 0 then
+    if vim.fn.search("{{.\\{-}}}") > 0 or vim.fn.search("{%.\\{-}%}") > 0 then
       vim.api.nvim_buf_set_option(0, 'filetype', 'terahtml')
     end
   end
