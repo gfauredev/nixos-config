d87dd11270fc2749482d9a3f6fa2ec294ee5762a
Guilhem Fauré <pro@gfaure.eu>
2024-02-21 16:17:57 +0100

    enable giada, ingen & surgeXT

--- home/module/audio.nix
+++ home/module/audio.nix
@@ -2,8 +2,9 @@
   home.packages = with pkgs; [
     # Tools
     klick # Metronome
-    # giada # Live electronic music platform
+    giada # Live electronic music platform
     carla # VST Host & audio router
+    ingen # Modular audio environment
     # helvum # PipeWire router
     # qpwgraph # PipeWire router
     # DAW & Editors
@@ -11,15 +12,14 @@
     ardour # Full fledged digital audio workstation
     # reaper # PROPRIETARY digital audio workstation
     # audacity # Simple audio editor
-    # ingen # Modular audio environment
     # VST
     distrho # Collection of audio plugins, contains Vitalium
     # helm # Ancestor of Vital
     # lsp-plugins # Collection of audio plugins
     calf # Collection of audio plugins
     # infamousPlugins # Collection of audio plugins
-    # surge-XT # Great hybrid substractive synth
-    # surge
+    surge-XT # Great hybrid substractive synth
+    # surge # Great hybrid substractive synth (old version)
     drumgizmo # High quality drums sampler
     # Misc
     yabridge # Use Windows VSTs with wine
