8c43deddba5c4143d13f6a34b22cd15f9410335a
Guilhem Fauré <pro@gfaure.eu>
2024-02-22 13:05:34 +0100

    clean

--- configuration.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, lib, config, pkgs, ... }: {
-  # Config file TEMPLATE
-  # TODO ensure that we need all of the above in every files
-
-  # Also prevents `nixos-generate-config` to write a same-named file
-}
--- secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  ninja = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6wm6k7PfjLQDlVoApJr954OjOl7+a35kS4QzEtQrgr"; # gf@ninja
-  gf = [ ninja ]; # All gf user instances
-in
-{
-  "home/gf/secret1.age".publicKeys = gf;
-}
