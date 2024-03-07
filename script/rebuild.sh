21d8b668305d72caadf12b949eb40af1c9fc9583
Guilhem Fauré <pro@gfaure.eu>
2024-03-07 15:15:16 +0100

    better rebuild script

--- script/rebuild.sh
+++ script/rebuild.sh
@@ -1,23 +1,64 @@
-if [ "$1" = "os" ] || [ "$1" = "system" ] || [ "$1" = "all" ]; then
-  printf "\nMounting /boot before system update"
-  sudo mount /boot # Use fstab
-  printf "\nPerforming system update"
-  sudo nixos-rebuild --flake . switch
-  printf "\nUnmounting /boot after update"
+CONFIG_DIR="$HOME/life.large/configuration.git/" # WARNING hardcoded config directory
+
+system_update() {
+  printf "\nMounting /boot before system update\n"
+  sudo mount /boot || return # Use fstab
+
+  printf "\nPerforming system update\n"
+  sudo nixos-rebuild --flake . switch || return
+
+  printf "\nUnmounting /boot after update\n"
   sudo umount /boot # Unmount for security
-fi
+}
 
-if [ "$1" != "os" ] && [ "$1" != "system" ]; then
-  printf "\nRemoving .config/mimeapps.list"
-  rm -f "$XDG_CONFIG_HOME/mimeapps.list" # Mitigate Mailspring behavior
-  printf "\nPerforming profile update"
-  home-manager --flake ".#${USER}@$(hostname)" switch
-fi
+home_update() {
+  printf "\nRemoving .config/mimeapps.list\n"
+  rm -f "$XDG_CONFIG_HOME/mimeapps.list" # Some apps replace it
 
-[ "$2" = "push" ] && git push
-[ "$2" = "commit" ] && git commit -am "Unnamed update" && git push
+  printf "\nPerforming profile update\n"
+  home-manager --flake ".#${USER}@$(hostname)" switch || return
+}
 
-[ "$3" = "off" ] && systemctl poweroff
-[ "$3" = "poweroff" ] && systemctl poweroff
+pushd "$CONFIG_DIR" || exit # Change to the config directory
 
-[ "$3" = "reboot" ] && systemctl reboot
+# Open what is configured within the default editor, then commit accordingly
+case "$1" in
+  "system")
+    $EDITOR system/ && git commit -am "System : $2" && system_update || exit
+    ;;
+  "all")
+    $EDITOR . && git commit -am "System & Home : $2" && system_update && home_update || exit
+    ;;
+  *)
+    $EDITOR home/ && git commit -am "Home : $2" && home_update || exit
+    ;;
+esac
+
+case "$3" in
+  "push")
+    git push
+    ;;
+  "off")
+    systemctl poweroff
+    ;;
+  "poweroff")
+    systemctl poweroff
+    ;;
+  "reboot")
+    systemctl reboot
+    ;;
+esac
+
+case "$4" in
+  "off")
+    systemctl poweroff
+    ;;
+  "poweroff")
+    systemctl poweroff
+    ;;
+  "reboot")
+    systemctl reboot
+    ;;
+esac
+
+popd || exit # Return to the starting directory
