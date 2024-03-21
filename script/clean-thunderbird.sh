2f6d8dcc4ff0e8945c21d54ba2bbc9b655fd74fb
Guilhem Fauré <pro@gfaure.eu>
2024-03-21 19:01:13 +0100

    fix: thunderbird clean script

--- script/clean-thunderbird.sh
+++ script/clean-thunderbird.sh
@@ -2,4 +2,4 @@ pgrep thunderbird && kill $(pgrep thunderbird) # Ensure Thunderbird is stopped
 
 trash "$HOME/.cache/thunderbird" # Remove Thunderbird’s general cache
 
-trash "$HOME/.thunderbird/pmptthhg.default/calendar-data" # Remove Calendar cache
+trash $HOME/.thunderbird/*.default/calendar-data # Remove Calendar cache
