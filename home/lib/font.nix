3e25b3efb1a9af95275adb870bacd3d91cedc2b6
Guilhem Fauré <pro@gfaure.eu>
2024-03-29 11:22:50 +0100

    feat: + nerd fonts, try to italic in wezterm

--- home/lib/font.nix
+++ home/lib/font.nix
@@ -10,23 +10,39 @@
     # noto-fonts-cjk-serif
     #################### Sans Fonts ####################
     fira-go # Great sans with icons
-    nacelle # Helvetica replacement
+    nacelle # Helvetica equivalent
     # inter # Interesting sans font
-    # carlito # Calibri replacement
+    # carlito # Calibri equivalent
     # merriweather-sans # Sans font readable on low res
     # libre-franklin
     # noto-fonts-cjk-sans
     #################### Mono Fonts ####################
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-    # fira-code # Great mono with ligatures & icons
+    (nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "FiraMono"
+        "Iosevka"
+        "IosevkaTerm"
+        "IosevkaTermSlab"
+        "Hack"
+        # "JetBrainsMono"
+        # "Noto"
+        # "SourceCodePro"
+        # "Ubuntu"
+        # "UbuntuMono"
+      ];
+    })
+    # fira-code # Great mono font
+    # fira-code-symbols # Great icons
+    # fira-code-nerdfont # Great mono with ligatures & icons
     #################### Fonts Packages ####################
-    # liberation_ttf # Times New Roman, Arial, Courier New
-    # nerdfonts # Mono fonts with lots of icons
-    # noto-fonts
+    # liberation_ttf # ≃ Times New Roman, Arial, Courier New equivalents
+    # nerdfonts # Big package of fonts with lots of icons
+    # noto-fonts # Google well internationalized fonts
     #################### Symbols Fonts ####################
-    lmmath # Classic font with math support
-    # emojione # Emoticons
-    # font-awesome # A lot of icons
+    # font-awesome # Thousands of icons
+    # lmmath # Classic font with math support
+    # emojione # Emojies
     # noto-fonts-emoji # Emojies
   ];
 }
