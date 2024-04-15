65f4e5f82b90ef7d05a6693bdd764566018a13d2
Guilhem Fauré <pro@gfaure.eu>
2024-04-15 13:01:41 +0200

    feat: webcamoid

--- home/media/photo.nix
+++ home/media/photo.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    # TODO use specific options when possible
     darktable # RAW pictures editing
     gimp # image editor
     inkscape # vector image editor
     # krita # image editor
+    webcamoid # Webcam capture
   ];
 }
