a43bdb4cc77acbc568d46a579f41d623562867ac
Guilhem Fauré <pro@gfaure.eu>
2024-05-21 19:01:31 +0200

    feat: vms install podman-compose alma

--- script/generic-virtual-machines/dnf.sh
+++ script/generic-virtual-machines/dnf.sh
@@ -4,5 +4,9 @@ dnf -y install podman podman-plugins podman-docker
 
 dnf -y install tcpdump sysstat git curl wget make tar gzip
 
+dnf -y install pip
+
+pip install podman-compose
+
 # dnf -y install epel-release
 # dnf -y install podman-compose
