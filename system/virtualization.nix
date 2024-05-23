c6bb5a5d9fba4ecf5b80b29f9e5837fb510e6a1d
Guilhem Fauré <pro@gfaure.eu>
2024-05-23 10:44:18 +0200

    feat: add Podman, reenable firewall
    squashme
    
    squashme

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -1,8 +1,18 @@
 { lib, pkgs, ... }: {
-  networking.firewall.enable = lib.mkForce false; # FIXME for virt nets
+  # networking.firewall.enable = lib.mkForce false; # FIXME for virt nets
 
   virtualisation = {
-    libvirtd = { enable = true; };
+    libvirtd = {
+      enable = true;
+      qemu.ovmf.enable = true;
+    };
+    podman = {
+      enable = true;
+      autoPrune.enable = true;
+      extraPackages = with pkgs; [ podman-compose gvisor ];
+      dockerSocket.enable = false;
+      dockerCompat = false;
+    };
     docker = {
       enable = true;
       extraPackages = with pkgs; [ docker-compose ];
