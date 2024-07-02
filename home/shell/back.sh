c76cb54d1fd553e102284e02143de26b368a2897
Guilhem Fauré <pro@gfaure.eu>
2024-07-03 00:37:15 +0200

    feat: back cmd exclude .stversions and .venv
    feat: back cmd exclude images, videos
    
    feat: back cmd exclude images, videos
    
    feat: back cmd exclude .thumbnails
    
    feat: light backup for multiboot sticks
    
    feat: light backup for multiboot sticks

--- home/shell/back.sh
+++ home/shell/back.sh
@@ -1,8 +1,37 @@
 #!zsh
+# Directories are, in order of importance (under user home)
+# 1. life
+# 2. project
+# 3. life.large
+# 4. project.large
+# 5. data
+# 6. data.large
+# 7. archive
+
+avail=$(\df --output=avail "$1"|tail -n1)
+echo "Available space on destination : $avail"
+used=$(\du -c $HOME/{data,life{,.large},project{,.large}}|tail -n1|cut -f1)
+echo "Used space by most important data : $used"
+
 if [[ "$1" == *"back"* ]]; then
-# Store everything incrementally with restic in backup drives which label contains "back"
-  restic -r "$1" -v backup $HOME/archive $HOME/data $HOME/data.large $HOME/life* $HOME/project*
+  # Backup everything incrementally with restic in backup drives (which label contains "back")
+  restic -r "$1" -v backup $HOME/{archive,data{,.large},life{,.large},project{,.large}}
+elif [ $avail -gt $used ]; then
+  # Store most important directories in drives or sticks (which label don’t contains "back")
+  rsync --verbose --archive --delete --human-readable \
+    --exclude=".stversions/" --exclude=".stfolder/" --exclude=".venv/" --exclude=".vagrant/" \
+    --exclude="*.png" --exclude="*.jpg" --exclude="*.jpeg" \
+    --exclude="*.PNG" --exclude="*.JPG" --exclude="*.JPEG" \
+    --exclude="*.mp4" --exclude="*.mkv" --exclude="*.avi" --exclude="*thumbnails*" \
+    --exclude="*.MP4" --exclude="*.MKV" --exclude="*.AVI" --exclude="*cache*" \
+    $HOME/{data,life{,.large},project{,.large}} "$1"
 else
-# Store most important directories in drives and sticks which label don’t contains "back"
-  rsync --verbose --archive --delete --human-readable $HOME/data $HOME/life $HOME/project "$1"
+  # Store less important directories in too small drives
+  rsync --verbose --archive --delete --human-readable \
+    --exclude=".stversions/" --exclude=".stfolder/" --exclude=".venv/" --exclude=".vagrant/" \
+    --exclude="*.png" --exclude="*.jpg" --exclude="*.jpeg" --exclude="*.webp" --exclude="*.avif" \
+    --exclude="*.PNG" --exclude="*.JPG" --exclude="*.JPEG" --exclude="*.WEBP" --exclude="*.AVIF" \
+    --exclude="*.mp4" --exclude="*.mkv" --exclude="*.avi" --exclude="*thumbnails*" \
+    --exclude="*.MP4" --exclude="*.MKV" --exclude="*.AVI" --exclude="*cache*" \
+    $HOME/{life,project} "$1"
 fi
