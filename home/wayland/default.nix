8b7913668092b664b855b1dd46debaccc425e42e
Guilhem Fauré <pro@gfaure.eu>
2024-07-04 03:42:52 +0200

    fix: egpu on tty2
    fix: egpu on tty2
    
    feat: use egpu if available

--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -29,6 +29,8 @@
       if [ -z "$DISPLAY" ]; then
         if [ "$XDG_VTNR" -eq 1 ]; then
           exec $HOME/.nix-profile/bin/Hyprland
+        elif [ "$XDG_VTNR" -eq 8 ]; then # Force use of eGPU
+          exec WLR_DRM_DEVICES=$HOME/.config/hypr/egpu $HOME/.nix-profile/bin/Hyprland
         elif [ "$XDG_VTNR" -eq 9 ]; then
           exec $HOME/.nix-profile/bin/niri --session
         fi
