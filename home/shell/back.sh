bfb6a1a367c4fa9b05efbee870aec2161abae309
Guilhem Fauré <pro@gfaure.eu>
2024-09-07 21:29:59 +0200

    squash

--- home/shell/back.sh
+++ home/shell/back.sh
@@ -2,14 +2,14 @@
 # Directories are, in order of importance (under user home)
 # 1. life, important documents of essentials area of life, like health or administrative
 # 2. project, files used towards a precise goal, usually with an end date
-# 3. life.large, same but too large to sync with phone
-# 4. project.large, same but too large to sync with phone
-# 5. data, files that might be interesting in the future
-# 6. data.large, same but too large to sync with phone
-# 7. archive, finished projects, eventually no more used "life" documents
+# 3. project.large, same but too large to sync with phone
+# 4. data, files that might be interesting in the future
+# 4. data.large, same but too large to sync with phone
+# 5. archive, finished projects, eventually no more used "life" documents
+# life.large does not exits as files are too important to not be synced with phone
 
 avail=$(\df --output=avail "$1"|tail -n1)
-used=$(\du -c $HOME/{data,life{,.large},project{,.large}}|tail -n1|cut -f1)
+used=$(\du -c $HOME/{data,life,project{,.large}}|tail -n1|cut -f1)
 echo "Available space on destination : ${avail}o"
 echo "Used space by important data :   ${used}o"
 
@@ -17,7 +17,7 @@ if [ $avail -gt $used ]; then
   if [[ "$1" == *"back"* ]]; then
     # Backup everything incrementally with restic in backup drives (which label contains "back")
     echo -e "Backing up everything with restic\n"
-    restic -r "$1" -v backup $HOME/{archive,data{,.large},life{,.large},project{,.large}} --exclude ".stversions/" --exclude ".stfolder/" --exclude ".venv*/" --exclude ".vagrant/"
+    restic -r "$1" -v backup $HOME/{archive,data{,.large},life,project{,.large}} --exclude ".stversions/" --exclude ".stfolder/" --exclude ".venv*/" --exclude ".vagrant/"
   else
     # Store most important directories in drives or sticks (which label don’t contains "back")
     echo -e "Backing up most important directories with rsync\n"
@@ -27,7 +27,7 @@ if [ $avail -gt $used ]; then
       --exclude="*.PNG" --exclude="*.JPG" --exclude="*.JPEG" \
       --exclude="*.mp4" --exclude="*.mkv" --exclude="*.avi" --exclude="*thumbnails*" \
       --exclude="*.MP4" --exclude="*.MKV" --exclude="*.AVI" --exclude="*cache*" \
-      $HOME/{data,life{,.large},project{,.large}} "$1"
+      $HOME/{data,life,project{,.large}} "$1"
   fi
 else
   # Store less important directories in too small drives
