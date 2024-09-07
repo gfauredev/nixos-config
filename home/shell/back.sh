c72d09e27163ca1e0f2bf045e7d26ad8ac564be7
Guilhem Fauré <pro@gfaure.eu>
2024-09-07 21:10:10 +0200

    squash

--- home/shell/back.sh
+++ home/shell/back.sh
@@ -10,17 +10,17 @@
 
 avail=$(\df --output=avail "$1"|tail -n1)
 used=$(\du -c $HOME/{data,life{,.large},project{,.large}}|tail -n1|cut -f1)
-echo "Available space on destination : $avail"
-echo "Used space by important data :   $used"
+echo "Available space on destination : ${avail}o"
+echo "Used space by important data :   ${used}o"
 
 if [ $avail -gt $used ]; then
   if [[ "$1" == *"back"* ]]; then
     # Backup everything incrementally with restic in backup drives (which label contains "back")
-    echo "Backing up everything with restic\n"
+    echo -e "Backing up everything with restic\n"
     restic -r "$1" -v backup $HOME/{archive,data{,.large},life{,.large},project{,.large}} --exclude ".stversions/" --exclude ".stfolder/" --exclude ".venv*/" --exclude ".vagrant/"
   else
     # Store most important directories in drives or sticks (which label don’t contains "back")
-    echo "Backing up most important directories with rsync\n"
+    echo -e "Backing up most important directories with rsync\n"
     rsync --verbose --archive --delete --human-readable --partial --progress \
       --exclude=".stversions/" --exclude=".stfolder/" --exclude=".venv*/" --exclude=".vagrant/" --exclude=".git/" \
       --exclude="*.png" --exclude="*.jpg" --exclude="*.jpeg" \
@@ -31,7 +31,7 @@ if [ $avail -gt $used ]; then
   fi
 else
   # Store less important directories in too small drives
-  echo "Backing up life/ and project/ directories with rsync\n"
+  echo -e "Backing up life/ and project/ directories with rsync\n"
   rsync --verbose --archive --delete --human-readable --partial --progress \
     --exclude=".stversions/" --exclude=".stfolder/" --exclude=".venv/" --exclude=".vagrant/" --exclude=".git/" \
     --exclude="*.png" --exclude="*.jpg" --exclude="*.jpeg" --exclude="*.webp" --exclude="*.avif" \
