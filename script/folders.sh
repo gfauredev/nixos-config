a01972b924b2b0d78f988c853249c47c0d88252f
Guilhem Fauré <pro@gfaure.eu>
2024-09-23 18:43:25 +0200

    refactor: slight dirs reorg

--- script/folders.sh
+++ script/folders.sh
@@ -12,41 +12,32 @@
 echo "# Project, projects and eventual nested sub-projects"
 echo "> A project directory contains files that might be required"
 echo "> to progress towards a final goal or a precise milestone"
-# if [ ! -d $HOME/project ]; then # TODO make this script idempotent
-mkdir -m 700 -v $HOME/project/
-# fi
-echo "#include stignore" > $HOME/project/.stignore
-echo "# Project, projects and eventual nested sub-projects"\
-  > $HOME/project/.ventoyignore
+mkdir -m 700 -v "$HOME/project/"
+echo "#include stignore" > "$HOME/project/.stignore"
+echo "# Project, projects and eventual nested sub-projects" > "$HOME/project/.ventoyignore"
 echo
 
 echo "# Life, life areas that requires monitoring through lifetime or recurring proofs/receipts"
 echo "> A life directory contains records for important areas that need monitoring (like health)"
 echo "> or documents that might be recurrently asked for (like identity proofs)"
-# if [ ! -d $HOME/life ]; then
 echo "- Replace house with address, bank with name of the bank, vehicle with model…"
-mkdir -m 700 -v $HOME/life/
-mkdir -m 700 -v $HOME/life/monitor/
-mkdir -m 700 -v $HOME/life/monitor/health/
-mkdir -m 700 -v $HOME/life/monitor/consumable/
-mkdir -m 700 -v $HOME/life/monitor/house/
-mkdir -m 700 -v $HOME/life/monitor/vehicle/
-mkdir -m 700 -v $HOME/life/monitor/bank/
-mkdir -m 700 -v $HOME/life/monitor/belonging/
-touch $HOME/life/monitor/GROUP_BY_PHYSICAL_OBJECT
+mkdir -m 700 -v "$HOME/life/"
+mkdir -m 700 -v "$HOME/life/monitor/"
+touch "$HOME/life/monitor/GROUP_BY_PHYSICAL_OBJECT"
+mkdir -m 700 -v "$HOME/life/monitor/body+health/"
+mkdir -m 700 -v "$HOME/life/monitor/42bashRoad+inventory/"
+mkdir -m 700 -v "$HOME/life/monitor/tuxCar1337/"
 echo "- The proofs are grouped by the organization that delivers them, non-grouped if several"
-mkdir -m 700 -v $HOME/life/proof+formalInfo/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/self/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/state/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/school/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/bank/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/insurance/
-mkdir -m 700 -v $HOME/life/proof+formalInfo/company/
-touch $HOME/life/proof+formalInfo/GROUP_BY_PROVIDER
-# fi
-echo "#include stignore" > $HOME/life/.stignore
-echo "# Life, life areas that requires monitoring through lifetime or recurring proofs/receipts"\
-  > $HOME/data/.ventoyignore
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/"
+touch "$HOME/life/proof+formalInfo/GROUP_BY_PROVIDER"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/self+family/"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/stateAdministration/"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/academy+nonprofit/"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/finance+insurance/"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/employer+client/"
+mkdir -m 700 -v "$HOME/life/proof+formalInfo/vendor+landlord/"
+echo "#include stignore" > "$HOME/life/.stignore"
+echo "# Life, life areas that requires monitoring through lifetime or recurring proofs/receipts" > "$HOME/data/.ventoyignore"
 echo
 
 echo "# Graph, a graph-like, non-hierarchical data store"
@@ -54,38 +45,28 @@ echo "> Contains linked and non-hierarchical data related to"
 echo "> projects, important life areas or anything that seems interesting, valuable or fun"
 echo "> The graph data store is usually used through and managed by a"
 echo "> dedicated knowledge-management app and synced with its own method"
-# if [ ! -d $HOME/.graph ]; then
-mkdir -m 700 -v $HOME/.graph/
-# fi
-# echo "#include stignore" > $HOME/.graph/.stignore
-echo "# Graph, a graph-like, non-hierarchical data store"
-  > $HOME/.graph/.ventoyignore
+mkdir -m 700 -v "$HOME/.graph/"
+echo "# Graph, a graph-like, non-hierarchical data store" > "$HOME/.graph/.ventoyignore"
 echo
 
 echo "# Data, unsorted or roughly sorted data that might be useful or interesting"
 echo "> A data directory contains files that might become useful"
 echo "> in a life area or in a project, or just be interesting or fun"
 echo "> Directories represents arbitrarily chosen grouping-criterions among many that might exist"
-# if [ ! -d $HOME/data ]; then
-mkdir -m 700 -v $HOME/data/
-mkdir -m 700 -v $HOME/data/dcim/
-mkdir -m 700 -v $HOME/data/screenshot/
-mkdir -m 700 -v $HOME/data/learn/
-mkdir -m 700 -v $HOME/data/memory/
-mkdir -m 700 -v $HOME/data/misc/
-# fi
-echo "#include stignore" > $HOME/data/.stignore
-echo "# Data, unsorted or roughly sorted data that might be useful or interesting"\
-  > $HOME/data/.ventoyignore
+mkdir -m 700 -v "$HOME/data/"
+mkdir -m 700 -v "$HOME/data/dcim/"
+mkdir -m 700 -v "$HOME/data/screenshot/"
+mkdir -m 700 -v "$HOME/data/learn/"
+mkdir -m 700 -v "$HOME/data/memory/"
+mkdir -m 700 -v "$HOME/data/misc/"
+echo "#include stignore" > "$HOME/data/.stignore"
+echo "# Data, unsorted or roughly sorted data that might be useful or interesting" > "$HOME/data/.ventoyignore"
 echo
 
 echo "Archive, definitively complete or discontinued project, expired or no longer useful files"
-# if [ ! -d $HOME/archive ]; then
-mkdir -m 700 -v $HOME/archive/
-mkdir -m 700 -v $HOME/archive/project/
-mkdir -m 700 -v $HOME/archive/life/
-# fi
-echo "#include stignore" > $HOME/archive/.stignore
-echo "Archive, definitively complete or discontinued project, expired or no longer useful files"\
-  > $HOME/archive/.ventoyignore
+mkdir -m 700 -v "$HOME/archive/"
+mkdir -m 700 -v "$HOME/archive/project/"
+mkdir -m 700 -v "$HOME/archive/life/"
+echo "#include stignore" > "$HOME/archive/.stignore"
+echo "Archive, definitively complete or discontinued project, expired or no longer useful files" > "$HOME/archive/.ventoyignore"
 echo
