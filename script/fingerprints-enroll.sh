7b9ef4ddbb06fdb844ff0e1ea7286bdf266e73e2
Guilhem Fauré <pro@gfaure.eu>
2024-10-10 08:31:14 +0200

    fix: fingerprints enroll

--- script/fingerprints-enroll.sh
+++ script/fingerprints-enroll.sh
@@ -1,4 +1,4 @@
-for finger in {left-middle-finger,left-index-finger,right-thumb,right-index-finger,right-middle-finger}; do
+for finger in left-middle-finger left-index-finger right-thumb right-index-finger right-middle-finger; do
   echo "PREPARE YOUR $finger, ENROLL IS COMING …"
   sleep 4
   sudo fprintd-enroll -f "$finger" gf
