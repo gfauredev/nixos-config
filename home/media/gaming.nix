512a391c96eeb4bb7f4be52c5e7308cecbb51d9e
Guilhem Fauré <pro@gfaure.eu>
2024-10-21 09:12:01 +0000

    fix: temporarily disable heroic

--- home/media/gaming.nix
+++ home/media/gaming.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    heroic # gog and epic games launcher
-    gogdl # gog module for heroic launcher
+    # heroic # gog and epic games launcher
+    # gogdl # gog module for heroic launcher
     legendary-gl # Epic games launcher alternative
     # moonlight-qt # Game streaming client # TEST if better than rustdesk
-    mangohud # A practical HUD on top of games
+    # mangohud # A practical HUD on top of games
     protonup-ng # Manage installation of proton-ge
   ];
 }
