cd57691b96f3c2712530e8b00cab27d0ac173e3d
Guilhem Fauré <pro@gfaure.eu>
2024-10-31 09:58:04 +0000

    feat(albert): extended desktop app search

--- home/wayland/launcher/albert/default.nix
+++ home/wayland/launcher/albert/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, term, ... }: {
+{ pkgs, ... }: {
   home.packages = with pkgs;
     [
       albert # Full-featured launcher
     ];
 
+  # NOTE disabled because albert modfies its own config
   xdg.configFile = {
     config = {
       enable = false;
