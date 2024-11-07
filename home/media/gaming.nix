ca964d54af319f0491aa9c05f8f5952fe8f78cd3
Guilhem Fauré <pro@gfaure.eu>
2024-11-07 14:19:55 +0100

    feat: heroic
    feat: heroic

--- home/media/gaming.nix
+++ home/media/gaming.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    # heroic # gog and epic games launcher
-    # gogdl # gog module for heroic launcher
-    legendary-gl # Epic games launcher alternative
+    heroic # Modern launcher supporting Gog and Epic Games
+    # gogdl # Gog alternative
+    # legendary-gl # Epic Games launcher alternative
+
     # moonlight-qt # Game streaming client # TEST if better than rustdesk
-    # mangohud # A practical HUD on top of games
-    protonup-ng # Manage installation of proton-ge
+    mangohud # A practical HUD on top of games
+    # protonup-ng # Manage installation of proton-ge
   ];
 }
