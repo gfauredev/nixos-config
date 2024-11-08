88a3adf8847b4d6848dd42d5e2ee115ea734a9c8
Guilhem Fauré <pro@gfaure.eu>
2024-11-08 09:06:34 +0100

    feat: add gimp plugins

--- home/media/photo.nix
+++ home/media/photo.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    darktable # RAW pictures editing
-    gimp # image editor
-    inkscape # vector image editor
-    # krita # image editor
+    darktable # RAW pictures editor
+    gimp-with-plugins # Raster image editor
+    inkscape # Vector image editor
+    # graphite # New vector and raster image editor
+    # krita # Raster image painting/drawing editor
     cheese # Webcam capture
   ];
 }
