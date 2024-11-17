0b7112456c2c421ec4a45bf2a4649038b6163504
Guilhem Fauré <pro@gfaure.eu>
2024-11-17 21:54:07 +0100

    fix: temporarily disable bespokesynth

--- home/media/audio.nix
+++ home/media/audio.nix
@@ -1,4 +1,4 @@
-{ pkgs, stablepkgs, ... }: {
+{ pkgs, ... }: {
 
   # pkgs.config.allowUnfreePredicate = pkg:
   #   builtins.elem (lib.getName pkg) [ "vital" ];
@@ -30,7 +30,7 @@
     # helm # Ancestor of Vital
     # surge-XT # Great hybrid substractive synth TODO reenable
     # surge # Great hybrid substractive synth (old version)
-    stablepkgs.bespokesynth-with-vst2 # Software modular synth with controllers support TODO reenable
+    # bespokesynth-with-vst2 # Software modular synth with controllers support TODO reenable
     drumgizmo # High quality drums sampler
     # Bridges & Drivers & Adapters
     # airwave # WINE-based VST bridge for Linux VST hosts
