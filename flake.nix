fb8c18db85fe7b790784866f273dc65804cca5c5
Guilhem Fauré <pro@gfaure.eu>
2024-11-26 11:27:58 +0100

    chore: rename stable->stablepkgs, remove when not needed

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # NixOS Unstable
     # unstable.url = "github:nixos/nixpkgs/f6950e6"; # Commit before 25.05 Unstable
     # stable.url = "github:nixos/nixpkgs/nixos-25.05"; # 25.05 NixOS Stable
-    stable.url = "github:nixos/nixpkgs/nixos-24.11"; # 24.11 NixOS Stable
+    stablepkgs.url = "github:nixos/nixpkgs/nixos-24.11"; # 24.11 NixOS Stable
     # stable.url = "github:nixos/nixpkgs/nixos-24.05"; # 24.05 NixOS Stable
 
     home-manager = {
@@ -20,7 +20,7 @@
     musnix.url = "github:musnix/musnix"; # Music production, audio optimizations
   };
 
-  outputs = { self, nixpkgs, stable, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosModules = {
       # Laptops #
       griffin = { # Griffin, a powerful and flying creature
@@ -48,7 +48,7 @@
       # Laptops #
       griffin = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux"; # PC architecture
-        specialArgs = { inherit inputs stable; };
+        specialArgs = { inherit inputs; };
         modules = [ self.nixosModules.griffin self.nixosModules.overlay ];
       };
       chimera = nixpkgs.lib.nixosSystem {
@@ -81,7 +81,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # TODO factorize
         extraSpecialArgs = {
           inherit inputs;
-          stablepkgs = import inputs.stable { # TODO do this cleaner
+          stablepkgs = import inputs.stablepkgs { # TODO do this cleaner
             system = "x86_64-linux"; # System architecture TODO factorize
             config.allowUnfree = true;
           };
@@ -96,7 +96,7 @@
       };
       "gf@chimera" = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # TODO factorize
-        extraSpecialArgs = { inherit inputs stable; };
+        extraSpecialArgs = { inherit inputs; };
         modules = [ # TODO clean this down to one module
           ./home/gf.nix # Myself’s home
           ./home/wayland/griffin.nix # Griffin’s GUI
