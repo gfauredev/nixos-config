61f05b778769e3f972f50139269ffa4ed4bbbf56
Guilhem Fauré <pro@gfaure.eu>
2024-12-03 17:01:08 +0100

    feat: add electronic circuit sim

--- home/media/science.nix
+++ home/media/science.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }: {
-  home.packages = with pkgs;
-    [
-      # kicad # Elecronics design
-      # fritzing # Elecronics design
+  home.packages = with pkgs; [
+    # kicad # Elecronics design
+    # fritzing # Elecronics design
+    xyce # Analog circuit simulator
+    # gnucap-full # Circuit analysis
+    # ngspice # Electronic circuit simulator
 
-      scilab-bin # Scientific computations
-    ];
+    scilab-bin # Scientific computations
+  ];
 }
 
