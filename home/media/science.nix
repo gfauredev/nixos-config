1d6867cf8af561c5ae906ba987005e62523a8faf
Guilhem Fauré <pro@gfaure.eu>
2024-12-03 17:08:51 +0100

    feat: gui for xyce

--- home/media/science.nix
+++ home/media/science.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs; [
     # kicad # Elecronics design
     # fritzing # Elecronics design
+    xschem # Schematic editor
     xyce # Analog circuit simulator
     # gnucap-full # Circuit analysis
     # ngspice # Electronic circuit simulator
