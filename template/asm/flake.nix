c438bfdece112c6c120f6daf14929e4c97b623ea
Guilhem Fauré <pro@gfaure.eu>
2024-12-09 16:02:02 +0100

    feat(template): assembly

---
+++ template/asm/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Assembly development environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ binutils nasm nasmfmt asm-lsp ];
+        };
+      };
+    };
+}
