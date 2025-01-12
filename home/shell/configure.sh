906f3494b7e798322ba265fe1499d405a3653225
Guilhem Fauré <pro@gfaure.eu>
2025-01-12 22:51:26 +0100

    fix(cfg): push rebase if ok

--- home/shell/configure.sh
+++ home/shell/configure.sh
@@ -253,7 +253,7 @@ if $push_repositories; then
   # Rebase only if not rebuilding and not updating and not turning off or rebooting
   if [ $rebuild_only = false ] && [ $system = false ] && [ $home = false ] &&
     [ $update_inputs = false ] && [ $amend_edits = false ] &&
-    [ $poweroff = false ] && [ $reboot = false ] && [ -n "$commit_message" ]; then
+    [ $poweroff = false ] && [ $reboot = false ] && [ -z "$commit_message" ]; then
     cfg_rebase
   fi
   cfg_push
