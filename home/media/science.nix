82af7e0f3706566010bda046294d5d72403b6c79
Guilhem Fauré <pro@gfaure.eu>
2025-01-29 12:17:31 +0100

    feat: librepcb

--- home/media/science.nix
+++ home/media/science.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    # kicad # Elecronics design
-    # fritzing # Elecronics design
-    xschem # Schematic editor
-    xyce # Analog circuit simulator
-    # gnucap-full # Circuit analysis
-    # ngspice # Electronic circuit simulator
+  home.packages = with pkgs;
+    [
+      librepcb # Modern EDA software
+      # kicad # Elecronics (EDA) software
+      # fritzing # Elecronics (EDA) software
 
-    scilab-bin # Scientific computations
-  ];
+      # xschem # Schematic editor TODO imperative install
+      # xyce # -parallel # Analog circuit simulator TODO imperative install
+
+      # gnucap-full # Circuit analysis
+      # ngspice # Electronic circuit simulator
+
+      # scilab-bin # Scientific computations TODO install imperatively
+    ];
 }
 
