9480d162c98c3e5f7fc98826d07720226a655599
Guilhem Fauré <pro@gfaure.eu>
2025-02-05 11:26:49 +0100

    feat(template): add valgrand and lizard to c

--- template/c-cpp/flake.nix
+++ template/c-cpp/flake.nix
@@ -1,36 +1,35 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
+  description = "C/C++ development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      # "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems
         (system: f { pkgs = import nixpkgs { inherit system; }; });
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell.override {
-          # Override stdenv in order to change compiler:
-          # stdenv = pkgs.clangStdenv;
+          # stdenv = pkgs.clangStdenv; # Change compiler
         } {
           packages = with pkgs; [
-            clang-tools
+            gdb
+            gnumake
             cmake
             codespell
-            conan
             cppcheck
+            conan
+            clang-tools
             doxygen
             gtest
             lcov
             vcpkg
             vcpkg-tool
-            gdb
-            gnumake
+            python312Packages.lizard
+            valgrind
           ];
-          # ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
         };
       });
     };
