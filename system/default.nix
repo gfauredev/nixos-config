16b842d3bbbb9680f99a30ecc698f024c24d0511
Guilhem Fauré <pro@gfaure.eu>
2025-02-19 09:20:23 +0100

    fix: remove comments in base nameservers

--- system/default.nix
+++ system/default.nix
@@ -116,50 +116,70 @@
   };
 
   networking = let
+    # TODO ensure DoH, DoT or better ODoH is used
+    # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
     dns0.open = [ # Unfiltered modern European public DNS
-      "2a0f:fc80::ffff#open.dns0.eu" # IPv6
-      "193.110.81.254#open.dns0.eu" # IPv4
-      "2a0f:fc81::ffff#open.dns0.eu" # IPv6
-      "185.253.5.254#open.dns0.eu" # IPv4
+      "2a0f:fc80::ffff" # open.dns0.eu" # IPv6
+      "193.110.81.254" # open.dns0.eu" # IPv4
+      "2a0f:fc81::ffff" # open.dns0.eu" # IPv6
+      "185.253.5.254" # open.dns0.eu" # IPv4
     ];
     dns0.normal = [ # Modern European public DNS
-      "2a0f:fc80::#dns0.eu" # IPv6
-      "193.110.81.0#dns0.eu" # IPv4
-      "2a0f:fc81::#dns0.eu" # IPv6
-      "185.253.5.0#dns0.eu" # IPv4
+      "2a0f:fc80::" # dns0.eu" # IPv6
+      "193.110.81.0" # dns0.eu" # IPv4
+      "2a0f:fc81::" # dns0.eu" # IPv6
+      "185.253.5.0" # dns0.eu" # IPv4
     ];
     dns0.hard = [ # Hardened modern European public DNS
-      "2a0f:fc80::9#zero.dns0.eu" # IPv6
-      "193.110.81.9#zero.dns0.eu" # IPv4
-      "2a0f:fc81::9#zero.dns0.eu" # IPv6
-      "185.253.5.9#zero.dns0.eu" # IPv4
+      "2a0f:fc80::9" # zero.dns0.eu" # IPv6
+      "193.110.81.9" # zero.dns0.eu" # IPv4
+      "2a0f:fc81::9" # zero.dns0.eu" # IPv6
+      "185.253.5.9" # zero.dns0.eu" # IPv4
     ];
-    fdn.normal = [ # Non-profit public DNS
-      "2001:910:800::12#ns0.fdn.fr"
-      "80.67.169.12#ns0.fdn.fr"
-      "2001:910:800::40#ns1.fdn.fr"
-      "80.67.169.40#ns1.fdn.fr"
+    fdn.open = [ # Non-profit public DNS
+      "2001:910:800::12" # ns0.fdn.fr" # IPv6
+      "80.67.169.12" # ns0.fdn.fr" # IPv4
+      "2001:910:800::40" # ns1.fdn.fr" # IPv6
+      "80.67.169.40" # ns1.fdn.fr" # IPv4
     ];
-    cloudflare.normal = [ # Quick public DNS
-      "2606:4700:4700::1111#one.one.one.one"
-      "1.1.1.1#one.one.one.one"
-      "2606:4700:4700::1001#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
+    cloudflare.open = [ # Quick public DNS
+      "2606:4700:4700::1111" # one.one.one.one" # IPv6
+      "1.1.1.1" # one.one.one.one" # IPv4
+      "2606:4700:4700::1001" # one.one.one.one" # IPv6
+      "1.0.0.1" # one.one.one.one" # IPv4
+    ];
+    quad9.open = [ # Public DNS
+      "2620:fe::10" # dns.quad9.net" # IPv6
+      "9.9.9.10" # dns.quad9.net" # IPv4
+      "2620:fe::fe:10" # dns.quad9.net" # IPv6
+      "149.112.112.10" # dns.quad9.net" # IPv4
     ];
     quad9.normal = [ # Public DNS
-      "2620:fe::fe#dns.quad9.net"
-      "9.9.9.9#dns.quad9.net"
-      "2620:fe::9#dns.quad9.net"
-      "149.112.112.112#dns.quad9.net"
+      "2620:fe::fe" # dns.quad9.net" # IPv6
+      "9.9.9.9" # dns.quad9.net" # IPv4
+      "2620:fe::9" # dns.quad9.net" # IPv6
+      "149.112.112.112" # dns.quad9.net" # IPv4
     ];
     quad9.hard = [ # Hardened public DNS
-      "2620:fe::11#dns11.quad9.net"
-      "9.9.9.11#dns11.quad9.net"
-      "2620:fe::fe:11#dns11.quad9.net"
-      "149.112.112.11#dns11.quad9.net"
+      "2620:fe::11" # dns11.quad9.net" # IPv6
+      "9.9.9.11" # dns11.quad9.net" # IPv4
+      "2620:fe::fe:11" # dns11.quad9.net" # IPv6
+      "149.112.112.11" # dns11.quad9.net" # IPv4
+    ];
+    mullvad.open = [ # Privacy oriented public DNS
+      "2a07:e340::2" # dns.mullvad.net # IPv6
+      "194.242.2.2" # dns.mullvad.net # IPv4
+    ];
+    mullvad.hard = [ # Privacy and security oriented public DNS
+      "2a07:e340::4" # base.dns.mullvad.net # IPv6
+      "194.242.2.4" # base.dns.mullvad.net # IPv4
+    ];
+    shaft.open = [
+      "2001:bc8:2c86:853::853" # dns.shaftinc.fr # IPv6
     ];
   in {
-    nameservers = dns0.open;
+    nameservers = dns0.open ++ cloudflare.open ++ quad9.open ++ fdn.open
+      ++ mullvad.hard ++ shaft.open;
     firewall.enable = lib.mkDefault true;
     wireguard.enable = lib.mkDefault true;
     networkmanager = {
