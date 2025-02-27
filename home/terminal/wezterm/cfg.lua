bd6a1bb26256f8d2a8cee5fcc05860610c0debf8
Guilhem Fauré <pro@gfaure.eu>
2025-02-27 20:05:36 +0100

    fix: force wezterm to use env $SHELL

--- home/terminal/wezterm/cfg.lua
+++ home/terminal/wezterm/cfg.lua
@@ -14,6 +14,7 @@ cfg = {
     top = 2,
     bottom = 2,
   },
+  default_prog = { "$SHELL" }
   -- enable_kitty_graphics = true, -- Kitty graphics protocol
   -- colors = { selection_bg = "#ffddcc" },
 }
