08fb0c9a30b91bbf40473880fa5b21901672df12
Guilhem Fauré <pro@gfaure.eu>
2025-02-28 13:19:44 +0100

    fix: temporarily disable nushell plugins

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -6,16 +6,16 @@
     configFile.source = ./config.nu;
     # envFile.source = ./env.nu; # TODO
     # loginFile.source = ./login.nu; # TODO
-    plugins = with pkgs.nushellPlugins; [
-      net # List network interfaces
+    plugins = with pkgs.nushellPlugins; [ # FIXME plugins don’t build
+      # net # List network interfaces
       skim # Fuzzy finder
       # dbus # Communicate with D-BUS
-      units # Convert between units
-      query # Query JSON, XML, web data
+      # units # Convert between units
+      # query # Query JSON, XML, web data
       gstat # Git status
       # polars # Work with polars dataframes
       # formats # Convert eml, ics, ini, vcf to Nushell tables
-      highlight # Syntax highlighting (like bat)
+      # highlight # Syntax highlighting (like bat)
     ];
   };
 }
