375e94901c6472c206e835942b4bfafb5a2ab659
Guilhem Fauré <pro@gfaure.eu>
2025-02-28 16:01:30 +0100

    fix: temporarily disable nu gstat

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -3,16 +3,17 @@
     enable = true;
     configFile.source = ./config.nu;
     # envFile.source = ./env.nu; # TODO
-    plugins = with pkgs.nushellPlugins; [ # FIXME plugins don’t build
-      # net # List network interfaces
-      skim # Fuzzy finder
-      # dbus # Communicate with D-BUS
-      # units # Convert between units
-      # query # Query JSON, XML, web data
-      gstat # Git status
-      # polars # Work with polars dataframes
-      # formats # Convert eml, ics, ini, vcf to Nushell tables
-      # highlight # Syntax highlighting (like bat)
-    ];
+    plugins = with pkgs.nushellPlugins; # FIXME plugins don’t build
+      [
+        # net # List network interfaces
+        skim # Fuzzy finder
+        # dbus # Communicate with D-BUS
+        # units # Convert between units
+        # query # Query JSON, XML, web data
+        # gstat # Git status
+        # polars # Work with polars dataframes
+        # formats # Convert eml, ics, ini, vcf to Nushell tables
+        # highlight # Syntax highlighting (like bat)
+      ];
   };
 }
