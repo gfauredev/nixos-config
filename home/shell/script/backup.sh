492e794264e199a70e2087bd01aa65219512ab91
Guilhem Fauré <pro@gfaure.eu>
2025-03-03 22:36:25 +0100

    fix: back encrypt for smaller devices

--- home/shell/script/backup.sh
+++ home/shell/script/backup.sh
@@ -7,8 +7,6 @@
 
 # Files corresponding to *.git/** or *.large should not be synced with lower capacity devices
 
-# TODO: restic backups in every drive of at least 500Go
-
 IMPORTANT="$HOME/life $HOME/project $HOME/.graph" # Always backed up
 DATA="$HOME/data"
 OS="$HOME/data/operatingSystems.large"
@@ -18,42 +16,47 @@ used=$(\du -c $IMPORTANT | tail -n1 | cut -f1) # Used by important dirs
 echo "Available space on destination : ${avail}o"
 echo "Used space by important data :   ${used}o"
 
-# Common rsync config
-alias rsync="rsync --verbose --archive --human-readable --partial --progress --exclude-from=$XDG_CONFIG_HOME/backup-exclude/common"
+rs() { # Custom rsync command
+  rsync --verbose --archive --human-readable --partial --progress \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/common \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img --exclude="*.large"
+}
 
 if [ "$avail" -gt "$used" ]; then
   case "$1" in
   *back*)
     # Backup everything incrementally with restic in backup drives
     # (which label contains "back")
-    printf "%s contains back : Backing up archive,data,graph,life,project with restic in it\n" "$1"
+    printf "%s contains back" "$1"
+    printf "Backing up life, project, data, archive incrementally with restic\n"
     restic -r "$1" -v backup $IMPORTANT "$DATA" "$ARCHIVE" \
       --exclude-caches --exclude-file="$XDG_CONFIG_HOME"/backup-exclude/common
     ;;
   *boot*)
     # Store most important directories in large drives or sticks
-    # (which label don’t contains "back")
-    printf "%s don’t contains back : Backing up data,graph,life,project with rsync in it\n" "$1"
-    rsync --exclude="*.large" \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
-      $IMPORTANT "$1"
-    rsync "$OS"/linux+bsd "$1"
-    rsync "$OS"/windows "$1"
+    # (which label don’t contains "back" but contains "boot")
+    printf "%s contains boot: " "$1"
+    printf "Backing up current life and project as well as bootables OSes\n"
+    # TODO encrypted archive of $IMPORTANT dirs, excluing common and img
+    printf "TODO with a crossplatform encrypted archiver like 7zip"
+    rs --delete "$OS"/linux+bsd "$1"
+    rs --delete "$OS"/windows "$1"
     ;;
   *)
     # Store most important directories in large drives or sticks
-    # (which label don’t contains "back")
-    printf "%s don’t contains back : Backing up data,graph,life,project with rsync in it\n" "$1"
-    rsync --exclude="*.large" \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
-      $IMPORTANT "$DATA" "$1"
+    # (which label don’t contains "back" nor "boot")
+    printf "%s don’t contains back: " "$1"
+    printf "Backing up life, project, data in encrypted archive\n"
+    # TODO encrypted archive of $IMPORTANT + $DATA dirs, excluing common and img
+    printf "TODO with a crossplatform encrypted archiver like 7zip"
     ;;
   esac
 else
   # Don’t store less important directories in too small drives
-  printf "%s seems almost full : Backing up graph,life,project with rsync in it\n" "$1"
-  rsync --delete --exclude="*.large" --exclude="*.git" \
-    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img $IMPORTANT "$1"
+  printf "%s seems almost full: " "$1"
+  printf "Backing up life and project in encrypted archive\n"
+  # TODO $IMPORTANT dirs encrypted archive, excluing common, img, *.large, *.git
+  printf "TODO with a crossplatform encrypted archiver like 7zip"
 fi
 
 printf "Don’t forget to clean the ~/archive directory with cleanarchive"
