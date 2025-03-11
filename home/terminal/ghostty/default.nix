85d085612cfd9793dca2f94731de7b62f3759156
Guilhem Fauré <pro@gfaure.eu>
2025-03-11 09:47:22 +0100

    feat: ghostty add close tab keybind

--- home/terminal/ghostty/default.nix
+++ home/terminal/ghostty/default.nix
@@ -8,8 +8,11 @@
   # See https://ghostty.org/docs/config
   programs.ghostty.settings = {
     keybind = [
-      "ctrl+t=new_tab"
-      "ctrl+s=next_tab"
+      "ctrl+t=new_split:down"
+      "ctrl+shift+t=new_tab"
+      "ctrl+s=goto_split:next"
+      "ctrl+shift+s=next_tab"
+      "ctrl+w=close_surface"
       "performable:ctrl+c=copy_to_clipboard"
       "performable:ctrl+v=paste_from_clipboard"
     ];
