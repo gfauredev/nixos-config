9e3da8815092d5465e3809df77e9f290277f2161
Guilhem Fauré <pro@gfaure.eu>
2025-03-17 12:04:36 +0100

    chore: remove albert bak

--- home/module/wayland/launcher/albert.bak/link.sh
+++
@@ -1,4 +0,0 @@
-# TODO execute post-activation
-dir=$(realpath "$(dirname "$0")")
-ln --force --symbolic "$dir"/config "$XDG_CONFIG_HOME"/albert/config
-ln --force --symbolic "$dir"/websearch "$XDG_CONFIG_HOME"/albert/websearch
