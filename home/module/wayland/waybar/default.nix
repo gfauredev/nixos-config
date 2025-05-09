b86922d983b0dc94cb56920bc14c15e694d46770
Guilhem Fauré <pro@gfaure.eu>
2025-05-09 14:16:20 +0200

    fix: temp input waybar

--- home/module/wayland/waybar/default.nix
+++ home/module/wayland/waybar/default.nix
@@ -32,17 +32,14 @@
             # max-length = 8;
           };
 
-          temperature = let
-            # hwmon = "4"; # TODO Nixos module options or better, dynamic
-            # temp = "1"; # TODO Nixos module options or better, dynamic
-            hwmon-paths = [
-              "/sys/class/hwmon/hwmon6/temp1_input"
-              "/sys/class/hwmon/hwmon4/temp1_input"
+          temperature = let # TODO Nixos module options or better, dynamic
+            griffin.cpu-temps = [
+              "/sys/devices/platform/coretemp.0/hwmon/hwmon6/temp1_input"
+              "/sys/devices/platform/coretemp.0/hwmon/hwmon4/temp1_input"
+              "/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input"
             ];
           in {
-            # thermal-zone = 0;
-            # hwmon-path = "/sys/class/hwmon/hwmon${hwmon}/temp${temp}_input";
-            hwmon-path = hwmon-paths;
+            hwmon-path = griffin.cpu-temps;
             critical-threshold = "75";
             format = "{icon} {temperatureC}";
             format-icons = [ "" "" "" "" "" ]; # FA
