c793c49afcada6a92045bfede1dd95f5d18ed253
Guilhem Fauré <pro@gfaure.eu>
2025-05-09 14:00:10 +0200

    fix: temp input waybar

--- home/module/wayland/waybar/default.nix
+++ home/module/wayland/waybar/default.nix
@@ -32,18 +32,20 @@
             # max-length = 8;
           };
 
-          temperature = let
-            hwmon = "4"; # TODO Nixos module options or better, dynamic
-            temp = "1"; # TODO Nixos module options or better, dynamic
-          in {
-            # thermal-zone = 0;
-            hwmon-path = "/sys/class/hwmon/hwmon${hwmon}/temp${temp}_input";
-            critical-threshold = "75";
-            format = "{icon} {temperatureC}";
-            format-icons = [ "" "" "" "" "" ]; # FA
-            tooltip = false;
-            # max-length = 6;
-          };
+          temperature =
+            # let
+            #   hwmon = "4"; # TODO Nixos module options or better, dynamic
+            #   temp = "1"; # TODO Nixos module options or better, dynamic
+            # in
+            {
+              # thermal-zone = 0;
+              # hwmon-path = "/sys/class/hwmon/hwmon${hwmon}/temp${temp}_input";
+              critical-threshold = "75";
+              format = "{icon} {temperatureC}";
+              format-icons = [ "" "" "" "" "" ]; # FA
+              tooltip = false;
+              # max-length = 6;
+            };
 
           cpu = {
             states = {
