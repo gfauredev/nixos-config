57e817e1ca56445d0436f5ac6c0bd7ce48babbe0
Guilhem Fauré <pro@gfaure.eu>
2025-05-26 17:42:41 +0200

    feat: add sqls

--- home/module/editor/helix/default.nix
+++ home/module/editor/helix/default.nix
@@ -199,6 +199,11 @@
           language-servers = [ "harper" "llm" ];
           auto-format = true;
         }
+        {
+          name = "sql";
+          language-servers = [ "sqls" ];
+          auto-format = true;
+        }
       ];
       language-server = {
         dprint = {
@@ -214,6 +219,7 @@
           command = "ruff";
           args = [ "server" ];
         };
+        sqls = { command = "sqls"; };
       };
     };
     themes = {
