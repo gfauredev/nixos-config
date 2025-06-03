898d1654388fcf4f408d2e835cecf29a88624b7c
Guilhem Fauré <pro@gfaure.eu>
2025-06-03 10:34:15 +0200

    fix: typ alias

--- home/module/shell/script/typ.sh
+++ home/module/shell/script/typ.sh
@@ -4,8 +4,14 @@ if [ "$#" -eq 0 ]; then
   typst compile "$(\ls --sort=time ./*.typ | head -n1)"
 elif [ "$#" -eq 1 ]; then
   # One main file passed: open preview on it, edit it, compiles it afterwards
-  tinymist preview "$1" &
+  # tinymist preview "$1" >/dev/null 2>&1 & # TODO
+  typst watch "$1" >/dev/null 2>&1 &
+  pid_compile=$!
+  zathura --fork "${1%.typ}.pdf" >/dev/null 2>&1 &
+  pid_preview=$!
   $EDITOR "$1"
+  kill $pid_compile
+  kill $pid_preview # Kill previously launched preview
   typst compile "$1"
 else
   # Several Typst files passed: compile them all
