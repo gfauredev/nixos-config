2a477855023c5e258ddac56b7fb554d60fd13ec5
Guilhem Fauré <pro@gfaure.eu>
2025-06-09 14:02:40 +0200

    fix: keyd bépo

--- system/module/remap.nix
+++ system/module/remap.nix
@@ -14,17 +14,18 @@
         default = {
           ids = [ "*" ];
           settings = let
+            # TODO BÉPO layout
             binds = {
-              # Helix-like motions (adatpted to BÉPO keyboard layout)
+              # Helix-like motions (adapted to BÉPO keyboard layout)
               h = "left";
               j = "down";
               k = "up";
               l = "right";
-              b = "C-S-left"; # Selects backwards until a whitespace
-              e = "C-S-right"; # Selects forwards until a whitespace
-              w = "C-S-right"; # Should select until letter preceded by space
+              q = "C-S-left"; # Selects backwards until a whitespace (b)
+              f = "C-S-right"; # Selects forwards until a whitespace (e)
+              "]" = "C-S-right"; # Should select until letter preceded by space
               # Other shortcuts
-              g = "oneshot(goto)"; # GO TO mode
+              "," = "oneshot(goto)"; # GO TO mode (g)
               enter = "menu";
               # "/" = "C-f"; # Search
             };
@@ -33,38 +34,38 @@
             main = {
               capslock = "overload(control, esc)";
               # "overload(control, timeout(oneshot(capslock), 80, esc))";
-              # - press space, < 500ms, release space: type space
-              # - press space, > 500ms, release space: noop
+              # - press space, < 800ms, release space: type space
+              # - press space, > 800ms, release space: noop
               # - press space, press a symbol: use a spacemode binding
               # - press a symbol, < 30ms, press space: type space
-              space = "overloadi(space, overloadt2(spacemode, space, 500), 30)";
+              space = "overloadi(space, overloadt2(spacemode, space, 800), 30)";
             };
             # Capslock within 80ms of previous capslock enables Helix mode
             # capslock.capslock = "toggle(helixmode)"; # TODO
             spacemode = binds; # Maintain space
             helixmode = binds // {
               # Exit this mode
-              i = "clear()"; # Return to insert mode (the default)
+              d = "clear()"; # Return to insert mode (the default)
               capslock = "clear()"; # Leave helix like mode (the default)
               esc = "clear()"; # Leave helix like mode (the default)
             };
             goto = {
-              c = "home"; # Go to the line’s beginning
-              r = "end"; # Go to the line’s end
-              g = "C-home"; # Go to the document’s beginning
-              e = "C-end"; # Go to the document’s end
+              h = "home"; # Go to the line’s beginning
+              l = "end"; # Go to the line’s end
+              "," = "C-home"; # Go to the document’s beginning (g)
+              f = "C-end"; # Go to the document’s end (e)
             };
           };
-          # c = S-home # Selects to the line’s beginning
-          # t = pagedown # One screen down
-          # s = pageup # One screen up
-          # r = S-end  # Selects to the line’s end
+          # h = S-home # Selects to the line’s beginning
+          # j = pagedown # One screen down
+          # k = pageup # One screen up
+          # l = S-end  # Selects to the line’s end
           extraConfig = ''
             [spacemode+shift]
-            c = S-home
-            t = pagedown
-            s = pageup
-            r = S-end
+            h=S-home
+            j=pagedown
+            k=pageup
+            l=S-end
           '';
         };
       };
