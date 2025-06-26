f211ca4a76f9893cd2e419076b0072ccee2f4b4a
Guilhem Fauré <pro@gfaure.eu>
2025-06-26 13:58:51 +0200

    fix: fonts

--- home/module/media/typst.nix
+++ home/module/media/typst.nix
@@ -6,20 +6,21 @@
     tinymist # Typst LSP
     typstyle # Typst formatter
     ## Serif fonts
-    # libre-baskerville # Great, stylish serif
+    libertinus
+    libre-baskerville # Great, stylish serif
     # vollkorn # Great serif font
     # merriweather # Serif readable on low res screens
     # gelasio # Serif Georgia replacement
     # noto-fonts-cjk-serif # Chinese, Japanese, Korean Serif
     ## Sans fonts
-    # aileron # Helvetica equivalent
+    aileron # Helvetica equivalent
     # nacelle # Helvetica equivalent
-    # inter # Interesting sans font
-    # fira-go # Great sans with icons
+    inter # Interesting sans font
+    fira-go # Great sans with icons
     # carlito # Calibri equivalent
     # noto-fonts-cjk-sans # Chinese, Japanese, Korean Sans
     ## Mono fonts
-    # nerd-fonts.jetbrains-mono
+    nerd-fonts.jetbrains-mono
     # nerd-fonts.fira-code
     ## Packages of multiple fonts
     # liberation_ttf # ≃ Times New Roman, Arial, Courier New equivalents
