b9f654277be74832ac3f024d4def119158f26afd
Guilhem Fauré <pro@gfaure.eu>
2025-08-08 00:57:15 +0200

    fix: doggo->dig

--- system/default.nix
+++ system/default.nix
@@ -1,7 +1,9 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
+{
   # TODO find and possibly disable the Python 3.12 service launched at boot
-  # 
-  nixpkgs.config.allowUnfreePredicate = pkg:
+  #
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
     builtins.elem (lib.getName pkg) [
       "nvidia-x11" # GPU drivers
       "nvidia-settings" # GPU drivers
@@ -47,8 +49,7 @@
       "kernel.sysrq" = 212; # kbd control, read-only remount, nicing, power
       "vm.swappiness" = 50; # Reduce memory swap to disk
     };
-    kernelPackages = lib.mkOverride 1001
-      pkgs.linuxPackages_latest; # Latest Linux kernel by default
+    kernelPackages = lib.mkOverride 1001 pkgs.linuxPackages_latest; # Latest Linux kernel by default
     kernelParams = [
       "threadirqs" # Process interrupts asynchronously to reduce latency
       "zswap.enabled=1" # TODO setup encrypted swap partition
@@ -65,95 +66,107 @@
 
   time = {
     timeZone = lib.mkDefault "Europe/Paris";
-    hardwareClockInLocalTime =
-      lib.mkDefault false; # True for compatibility with Windows
+    hardwareClockInLocalTime = lib.mkDefault false; # True for compatibility with Windows
   };
 
-  networking = let
-    # TODO ensure DoH, DoT or better ODoH is used
-    # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-    dns0.open = [ # Unfiltered modern European public DNS
-      "2a0f:fc80::ffff" # open.dns0.eu" # IPv6
-      "193.110.81.254" # open.dns0.eu" # IPv4
-      "2a0f:fc81::ffff" # open.dns0.eu" # IPv6
-      "185.253.5.254" # open.dns0.eu" # IPv4
-    ];
-    dns0.normal = [ # Modern European public DNS
-      "2a0f:fc80::" # dns0.eu" # IPv6
-      "193.110.81.0" # dns0.eu" # IPv4
-      "2a0f:fc81::" # dns0.eu" # IPv6
-      "185.253.5.0" # dns0.eu" # IPv4
-    ];
-    dns0.hard = [ # Hardened modern European public DNS
-      "2a0f:fc80::9" # zero.dns0.eu" # IPv6
-      "193.110.81.9" # zero.dns0.eu" # IPv4
-      "2a0f:fc81::9" # zero.dns0.eu" # IPv6
-      "185.253.5.9" # zero.dns0.eu" # IPv4
-    ];
-    fdn.open = [ # Non-profit public DNS
-      "2001:910:800::12" # ns0.fdn.fr" # IPv6
-      "80.67.169.12" # ns0.fdn.fr" # IPv4
-      "2001:910:800::40" # ns1.fdn.fr" # IPv6
-      "80.67.169.40" # ns1.fdn.fr" # IPv4
-    ];
-    cloudflare.open = [ # Quick public DNS
-      "2606:4700:4700::1111" # one.one.one.one" # IPv6
-      "1.1.1.1" # one.one.one.one" # IPv4
-      "2606:4700:4700::1001" # one.one.one.one" # IPv6
-      "1.0.0.1" # one.one.one.one" # IPv4
-    ];
-    quad9.open = [ # Public DNS
-      "2620:fe::10" # dns.quad9.net" # IPv6
-      "9.9.9.10" # dns.quad9.net" # IPv4
-      "2620:fe::fe:10" # dns.quad9.net" # IPv6
-      "149.112.112.10" # dns.quad9.net" # IPv4
-    ];
-    quad9.normal = [ # Public DNS
-      "2620:fe::fe" # dns.quad9.net" # IPv6
-      "9.9.9.9" # dns.quad9.net" # IPv4
-      "2620:fe::9" # dns.quad9.net" # IPv6
-      "149.112.112.112" # dns.quad9.net" # IPv4
-    ];
-    quad9.hard = [ # Hardened public DNS
-      "2620:fe::11" # dns11.quad9.net" # IPv6
-      "9.9.9.11" # dns11.quad9.net" # IPv4
-      "2620:fe::fe:11" # dns11.quad9.net" # IPv6
-      "149.112.112.11" # dns11.quad9.net" # IPv4
-    ];
-    mullvad.open = [ # Privacy oriented public DNS
-      "2a07:e340::2" # dns.mullvad.net # IPv6
-      "194.242.2.2" # dns.mullvad.net # IPv4
-    ];
-    mullvad.hard = [ # Privacy and security oriented public DNS
-      "2a07:e340::4" # base.dns.mullvad.net # IPv6
-      "194.242.2.4" # base.dns.mullvad.net # IPv4
-    ];
-    shaft.open = [
-      "2001:bc8:2c86:853::853" # dns.shaftinc.fr # IPv6
-    ];
-  in {
-    nameservers = dns0.open ++ cloudflare.open ++ quad9.open ++ fdn.open
-      ++ mullvad.hard ++ shaft.open;
-    firewall.enable = lib.mkDefault true;
-    wireguard.enable = lib.mkDefault true;
-    networkmanager = {
-      # See: https://developer.gnome.org/NetworkManager/stable/NetworkManager.html
-      enable = lib.mkDefault true;
-      # insertNameservers = config.networking.nameservers;
-      # dns = "none"; # Static name servers, we don’t want DHCP ones
-      dispatcherScripts = [{
-        source = pkgs.writeShellScript "09-timezone" ''
-          case "$2" in
-          # connectivity-change) # Prevent change with VPNs
-          up)
-            timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
-            ;;
-          esac
-        '';
-        type = "basic";
-      }];
+  networking =
+    let
+      # TODO ensure DoH, DoT or better ODoH is used
+      # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      dns0.open = [
+        # Unfiltered modern European public DNS
+        "2a0f:fc80::ffff" # open.dns0.eu" # IPv6
+        "193.110.81.254" # open.dns0.eu" # IPv4
+        "2a0f:fc81::ffff" # open.dns0.eu" # IPv6
+        "185.253.5.254" # open.dns0.eu" # IPv4
+      ];
+      dns0.normal = [
+        # Modern European public DNS
+        "2a0f:fc80::" # dns0.eu" # IPv6
+        "193.110.81.0" # dns0.eu" # IPv4
+        "2a0f:fc81::" # dns0.eu" # IPv6
+        "185.253.5.0" # dns0.eu" # IPv4
+      ];
+      dns0.hard = [
+        # Hardened modern European public DNS
+        "2a0f:fc80::9" # zero.dns0.eu" # IPv6
+        "193.110.81.9" # zero.dns0.eu" # IPv4
+        "2a0f:fc81::9" # zero.dns0.eu" # IPv6
+        "185.253.5.9" # zero.dns0.eu" # IPv4
+      ];
+      fdn.open = [
+        # Non-profit public DNS
+        "2001:910:800::12" # ns0.fdn.fr" # IPv6
+        "80.67.169.12" # ns0.fdn.fr" # IPv4
+        "2001:910:800::40" # ns1.fdn.fr" # IPv6
+        "80.67.169.40" # ns1.fdn.fr" # IPv4
+      ];
+      cloudflare.open = [
+        # Quick public DNS
+        "2606:4700:4700::1111" # one.one.one.one" # IPv6
+        "1.1.1.1" # one.one.one.one" # IPv4
+        "2606:4700:4700::1001" # one.one.one.one" # IPv6
+        "1.0.0.1" # one.one.one.one" # IPv4
+      ];
+      quad9.open = [
+        # Public DNS
+        "2620:fe::10" # dns.quad9.net" # IPv6
+        "9.9.9.10" # dns.quad9.net" # IPv4
+        "2620:fe::fe:10" # dns.quad9.net" # IPv6
+        "149.112.112.10" # dns.quad9.net" # IPv4
+      ];
+      quad9.normal = [
+        # Public DNS
+        "2620:fe::fe" # dns.quad9.net" # IPv6
+        "9.9.9.9" # dns.quad9.net" # IPv4
+        "2620:fe::9" # dns.quad9.net" # IPv6
+        "149.112.112.112" # dns.quad9.net" # IPv4
+      ];
+      quad9.hard = [
+        # Hardened public DNS
+        "2620:fe::11" # dns11.quad9.net" # IPv6
+        "9.9.9.11" # dns11.quad9.net" # IPv4
+        "2620:fe::fe:11" # dns11.quad9.net" # IPv6
+        "149.112.112.11" # dns11.quad9.net" # IPv4
+      ];
+      mullvad.open = [
+        # Privacy oriented public DNS
+        "2a07:e340::2" # dns.mullvad.net # IPv6
+        "194.242.2.2" # dns.mullvad.net # IPv4
+      ];
+      mullvad.hard = [
+        # Privacy and security oriented public DNS
+        "2a07:e340::4" # base.dns.mullvad.net # IPv6
+        "194.242.2.4" # base.dns.mullvad.net # IPv4
+      ];
+      shaft.open = [
+        "2001:bc8:2c86:853::853" # dns.shaftinc.fr # IPv6
+      ];
+    in
+    {
+      nameservers = dns0.open ++ cloudflare.open ++ quad9.open ++ fdn.open ++ mullvad.hard ++ shaft.open;
+      firewall.enable = lib.mkDefault true;
+      wireguard.enable = lib.mkDefault true;
+      networkmanager = {
+        # See: https://developer.gnome.org/NetworkManager/stable/NetworkManager.html
+        enable = lib.mkDefault true;
+        # insertNameservers = config.networking.nameservers;
+        # dns = "none"; # Static name servers, we don’t want DHCP ones
+        dispatcherScripts = [
+          {
+            source = pkgs.writeShellScript "09-timezone" ''
+              case "$2" in
+              # connectivity-change) # Prevent change with VPNs
+              up)
+                timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
+                ;;
+              esac
+            '';
+            type = "basic";
+          }
+        ];
+      };
     };
-  };
 
   hardware = {
     bluetooth = {
@@ -185,8 +198,7 @@
           ];
           cache_file = "public-resolvers.md";
           # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-          minisign_key =
-            "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         };
         ipv4_servers = true;
         ipv6_servers = true;
@@ -218,8 +230,8 @@
       wakelan # send magick packet to wake WoL devices
       sshfs # browser ssh as directory
       rsync # cp through network & with superpowers
-      doggo # Modern CLI DNS client
-      # dig # DNS analyzer
+      # doggo # Modern CLI DNS client
+      dig # DNS analyzer
       gping # Ping with a graph
       tcpdump # Dump network packets
       dhcpdump # DHCP debugging
