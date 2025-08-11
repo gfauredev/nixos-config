78d55972f118eb450cada2aa3c5c9a8b0021e32c
Guilhem Fauré <pro@gfaure.eu>
2025-08-11 23:30:35 +0200

    fix: temporarily disable steam run, gnucash

--- home/module/shell/default.nix
+++ home/module/shell/default.nix
@@ -1,71 +1,70 @@
-{ lib, pkgs, ... }: {
-  imports = [ ./alias.nix ./nushell ./zsh.nix ./pulsemixer ];
-  home.packages = let
-    init-dev-env =
-      pkgs.writeScriptBin "dev" "${lib.readFile ./script/init-dev-env.sh}";
-    smart-terminal =
-      pkgs.writeScriptBin "t" "${lib.readFile ./script/smart-terminal.sh}";
-    extract = pkgs.writeScriptBin "ex" "${lib.readFile ./script/extract.sh}";
-    backup = pkgs.writeScriptBin "back" "${lib.readFile ./script/backup.sh}";
-    clean-archive = pkgs.writeScriptBin "cleanarchive"
-      "${lib.readFile ./script/clean-archive.sh}";
-    configure =
-      pkgs.writeScriptBin "configure" "${lib.readFile ./script/configure.sh}";
-    smart-commit =
-      pkgs.writeScriptBin "cmt" "${lib.readFile ./script/smart-commit.sh}";
-    date-edit =
-      pkgs.writeScriptBin "de" "${lib.readFile ./script/date-edit.sh}";
-    present-pdf =
-      pkgs.writeScriptBin "present" "${lib.readFile ./script/present.sh}";
-    typst-compile = pkgs.writeScriptBin "typ" "${lib.readFile ./script/typ.sh}";
-    usb-mount =
-      pkgs.writeScriptBin "mount.usb" "${lib.readFile ./script/usb.sh}";
-    mtp-mount =
-      pkgs.writeScriptBin "mount.mtp" "${lib.readFile ./script/mtp.sh}";
-    archive =
-      pkgs.writeScriptBin "archive" "${lib.readFile ./script/archive.sh}";
-  in with pkgs; [
-    # Custom scripts
-    init-dev-env # Initialize a Nix Flake based development environment
-    smart-terminal # Open a terminal quickly with first parameter always cd
-    extract # Extract any compressed file
-    backup # Backup with restic or rsync
-    clean-archive # Clean the ~/archive dir after a backup
-    configure # Configure this flake config
-    smart-commit # Quickly commit or amend, lint message
-    date-edit # Open text files, prepend current date to the first one
-    present-pdf # Open detached pdfpc to present a PDF slide
-    typst-compile # Compile the latest edited Typst file in current dir
-    usb-mount # Quickly mount or unmount a USB device in ~/usb
-    mtp-mount # Quickly mount or unmount Android device(s) mounted in ~/mtp
-    archive # Quickly move a directory inside ~/archive/
-    # Development and general CLI tools
-    ov # Modern pager
-    trash-cli # Manage a trash from CLI
-    ripgrep-all # ripgrep for non-text files
-    duf # global disk usage
-    du-dust # detailed disk usage of a directory
-    hexyl # hex viever
-    sd # Intuitive find & replace
-    grex # Regex generator from test cases
-    moreutils # Additional Unix utilities
-    # rustdesk # Modern remote desktop
-    kalker # Evaluate math expression
-    nixpkgs-review # Quickly review pull requests to nixpkgs TEST
-    comma # Run any command from Nixpkgs
-    steam-run # Run in isolated FHS
-    manix # Nix documentation CLI
-    watchexec # Run command when file changes
-    hyperfine # Benchmark commands
-    nickel # Modern configuration Nickel, Nix improvement
-    cdrkit # ISO tools and misc
-    browsh # 6ixel CLI web browser
-    pipectl # Named pipes management
-    # Git
-    gitlab-shell # GitLab CLI
-    gitmoji-cli # Use emojis in commit messages
-    commitlint-rs # Be consistent in commit messages
+{ lib, pkgs, ... }:
+{
+  imports = [
+    ./alias.nix
+    ./nushell
+    ./zsh.nix
+    ./pulsemixer
   ];
+  home.packages =
+    let
+      init-dev-env = pkgs.writeScriptBin "dev" "${lib.readFile ./script/init-dev-env.sh}";
+      smart-terminal = pkgs.writeScriptBin "t" "${lib.readFile ./script/smart-terminal.sh}";
+      extract = pkgs.writeScriptBin "ex" "${lib.readFile ./script/extract.sh}";
+      backup = pkgs.writeScriptBin "back" "${lib.readFile ./script/backup.sh}";
+      clean-archive = pkgs.writeScriptBin "cleanarchive" "${lib.readFile ./script/clean-archive.sh}";
+      configure = pkgs.writeScriptBin "configure" "${lib.readFile ./script/configure.sh}";
+      smart-commit = pkgs.writeScriptBin "cmt" "${lib.readFile ./script/smart-commit.sh}";
+      date-edit = pkgs.writeScriptBin "de" "${lib.readFile ./script/date-edit.sh}";
+      present-pdf = pkgs.writeScriptBin "present" "${lib.readFile ./script/present.sh}";
+      typst-compile = pkgs.writeScriptBin "typ" "${lib.readFile ./script/typ.sh}";
+      usb-mount = pkgs.writeScriptBin "mount.usb" "${lib.readFile ./script/usb.sh}";
+      mtp-mount = pkgs.writeScriptBin "mount.mtp" "${lib.readFile ./script/mtp.sh}";
+      archive = pkgs.writeScriptBin "archive" "${lib.readFile ./script/archive.sh}";
+    in
+    with pkgs;
+    [
+      # Custom scripts
+      init-dev-env # Initialize a Nix Flake based development environment
+      smart-terminal # Open a terminal quickly with first parameter always cd
+      extract # Extract any compressed file
+      backup # Backup with restic or rsync
+      clean-archive # Clean the ~/archive dir after a backup
+      configure # Configure this flake config
+      smart-commit # Quickly commit or amend, lint message
+      date-edit # Open text files, prepend current date to the first one
+      present-pdf # Open detached pdfpc to present a PDF slide
+      typst-compile # Compile the latest edited Typst file in current dir
+      usb-mount # Quickly mount or unmount a USB device in ~/usb
+      mtp-mount # Quickly mount or unmount Android device(s) mounted in ~/mtp
+      archive # Quickly move a directory inside ~/archive/
+      # Development and general CLI tools
+      ov # Modern pager
+      trash-cli # Manage a trash from CLI
+      ripgrep-all # ripgrep for non-text files
+      duf # global disk usage
+      du-dust # detailed disk usage of a directory
+      hexyl # hex viever
+      sd # Intuitive find & replace
+      grex # Regex generator from test cases
+      moreutils # Additional Unix utilities
+      # rustdesk # Modern remote desktop
+      kalker # Evaluate math expression
+      nixpkgs-review # Quickly review pull requests to nixpkgs TEST
+      comma # Run any command from Nixpkgs
+      # steam-run # Run in isolated FHS
+      manix # Nix documentation CLI
+      watchexec # Run command when file changes
+      hyperfine # Benchmark commands
+      nickel # Modern configuration Nickel, Nix improvement
+      cdrkit # ISO tools and misc
+      browsh # 6ixel CLI web browser
+      pipectl # Named pipes management
+      # Git
+      gitlab-shell # GitLab CLI
+      gitmoji-cli # Use emojis in commit messages
+      commitlint-rs # Be consistent in commit messages
+    ];
 
   # home.shell.enableShellIntegration = true; # Default
   # services.pueue.enable = true; # Maybe necessary to add pkg
@@ -97,8 +96,7 @@
           image = "archlinux";
           # image = "alpine:3";
           # entry = true; # ?
-          additional_packages =
-            "mesa uv ruff python-lsp-server python-rope helix";
+          additional_packages = "mesa uv ruff python-lsp-server python-rope helix";
           init_hooks = ''
             cd
             uv init
@@ -110,15 +108,17 @@
     broot.settings = {
       default_flags = "dgps";
       special_paths = lib.mkForce { }; # Remove them
-      verbs = [{
-        invocation = "edit";
-        key = "enter";
-        shortcut = "e";
-        external = "$EDITOR {file}:{line}";
-        apply_to = "text_file";
-        leave_broot = false;
-        set_working_dir = true;
-      }];
+      verbs = [
+        {
+          invocation = "edit";
+          key = "enter";
+          shortcut = "e";
+          external = "$EDITOR {file}:{line}";
+          apply_to = "text_file";
+          leave_broot = false;
+          set_working_dir = true;
+        }
+      ];
     };
     atuin.settings = {
       enter_accept = true; # Don’t currently work on Nushell
