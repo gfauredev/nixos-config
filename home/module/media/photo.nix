54c67ad9cc4412b9f796c1daf84dd3e47aede9b5
Guilhem Fauré <pro@gfaure.eu>
2025-08-11 20:37:58 +0200

    fix: temporarily disable inkscape

--- home/module/media/photo.nix
+++ home/module/media/photo.nix
@@ -1,8 +1,9 @@
-{ pkgs, stablepkgs, ... }: {
+{ pkgs, stablepkgs, ... }:
+{
   home.packages = with pkgs; [
-    darktable # RAW pictures editor
+    # darktable # RAW pictures editor TODO reenable
     stablepkgs.gimp-with-plugins # Raster image editor
-    inkscape # Vector image editor
+    # inkscape # Vector image editor TODO reenable
     # graphite # New vector and raster image editor
     # krita # Raster image painting/drawing editor
     cheese # Webcam capture
