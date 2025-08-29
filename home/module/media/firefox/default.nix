4bf97c51195a90b7bd3aa9ab701da99595a7f7a3
Guilhem Fauré <pro@gfaure.eu>
2025-08-30 01:57:26 +0200

    feat: firefox settings

--- home/module/media/firefox/default.nix
+++ home/module/media/firefox/default.nix
@@ -40,7 +40,7 @@
         };
         "jid1-MnnxcxisBPnSXQ@jetpack" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-          installation_mode = "normal_installed";
+          installation_mode = "allow";
           private_browsing = true;
         };
       };
@@ -153,6 +153,10 @@
         # "Arch Wiki" # TODO
       ];
       settings = {
+        "sidebar.main.tools" = "aichat,syncedtabs,history,bookmarks";
+        "sidebar.notification.badge.aichat" = false;
+        "sidebar.revamp" = true;
+        "sidebar.verticalTabs" = true;
         # Style settings from gwfox (Zen Browser like style)
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "svg.context-properties.content.enabled" = true;
