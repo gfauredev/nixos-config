e5a2cc03052145a8539670bf07736ddb766e165f
Guilhem Fauré <pro@gfaure.eu>
2025-08-29 22:17:32 +0200

    fix: temporarily disable kdenlive

--- home/module/media/video.nix
+++ home/module/media/video.nix
@@ -3,8 +3,8 @@
   home.packages = with pkgs; [
     # losslesscut-bin # Basic lossless video edditing using ffmpeg
 
-    glaxnimate # video editing library used by kdenlive
-    libsForQt5.kdenlive # simple video editor (works on nvidia)
+    # libsForQt5.kdenlive # simple video editor (works on nvidia) FIXME
+    # glaxnimate # video editing library used by kdenlive
     # shotcut # video editor
     # flowblade # non linear video editor
     # olive-editor # non linear video editor
