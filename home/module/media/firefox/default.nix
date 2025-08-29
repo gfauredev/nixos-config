e984372f8b23b43374d849689f2dc268562b6305
Guilhem Fauré <pro@gfaure.eu>
2025-08-30 01:40:47 +0200

    fix: extensions policies, gwfox settings

--- home/module/media/firefox/default.nix
+++ home/module/media/firefox/default.nix
@@ -13,32 +13,32 @@
       BlockAboutConfig = true;
       DefaultDownloadDirectory = config.home.sessionVariables.XDG_DOWNLOAD_DIR;
       ExtensionSettings = {
-        uBlockOrigin = {
+        "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
-        protonPass = {
+        "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
           installation_mode = "normal_installed";
           private_browsing = true;
         };
-        darkReader = {
+        "addon@darkreader.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
           installation_mode = "normal_installed";
           private_browsing = true;
         };
-        languageTool = {
+        "languagetool-webextension@languagetool.org" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/languagetool/latest.xpi";
           installation_mode = "normal_installed";
           private_browsing = true;
         };
-        clearUrls = {
+        "{74145f27-f039-47ce-a470-a662b129930a}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
           installation_mode = "normal_installed";
           private_browsing = true;
         };
-        privacyBadger = {
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
           installation_mode = "normal_installed";
           private_browsing = true;
@@ -152,9 +152,21 @@
         "nixwiki"
         # "Arch Wiki" # TODO
       ];
+      settings = {
+        # Style settings from gwfox (Zen Browser like style)
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "widget.gtk.rounded-bottom-corners.enabled" = true;
+        "sidebar.animation.enabled" = false;
+        # "browser.startup.homepage" = "";
+        # "browser.search.region" = "GB";
+        # "browser.search.isUS" = false;
+        # "distribution.searchplugins.defaultLocale" = "en-GB";
+        # "general.useragent.locale" = "en-GB";
+        # "browser.bookmarks.showMobileBookmarks" = true;
+      };
       userChrome = lib.readFile ./userChrome.css;
       userContent = lib.readFile ./userContent.css;
-      # TEST if better than with policies
       # extensions = {
       #   force = true; # WARN overrides stateful settings
       #   packages = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -174,21 +186,6 @@
       #     # See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/permissions
       #     permissions = [ "activeTab" ];
       #   };
-      # };
-      # TEST if useful
-      # settings = {
-      #   "browser.startup.homepage" = "";
-      #   "browser.search.region" = "GB";
-      #   "browser.search.isUS" = false;
-      #   "distribution.searchplugins.defaultLocale" = "en-GB";
-      #   "general.useragent.locale" = "en-GB";
-      #   "browser.bookmarks.showMobileBookmarks" = true;
-      #   "browser.newtabpage.pinned" = [
-      #     {
-      #       title = "NixOS";
-      #       url = "https://nixos.org";
-      #     }
-      #   ];
       # };
     };
   };
