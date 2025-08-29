0cfa7aa319409a0580590dc5228f8872a5b0c707
Guilhem Fauré <pro@gfaure.eu>
2025-08-29 23:14:13 +0200

    docs: add todo firefox

--- home/module/media/internet+communication.nix
+++ home/module/media/internet+communication.nix
@@ -49,6 +49,43 @@
   programs = {
     firefox = {
       enable = true; # Web browser
+      # profiles = { # TODO with custom userChrome.css + userContent.css
+      #   "user" = {
+      #     id = 0;
+      #     isDefault = true;
+      #     search.engines = {
+      #       "Nix Packages" = {
+      #         urls = [
+      #           {
+      #             template = "https://search.nixos.org/packages";
+      #             params = [
+      #               {
+      #                 name = "query";
+      #                 value = "{searchTerms}";
+      #               }
+      #             ];
+      #           }
+      #         ];
+      #         icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      #         definedAliases = [ "@np" ];
+      #       };
+      #       "Nix Options" = {
+      #         definedAliases = [ "@no" ];
+      #         urls = [
+      #           {
+      #             template = "https://search.nixos.org/options";
+      #             params = [
+      #               {
+      #                 name = "query";
+      #                 value = "{searchTerms}";
+      #               }
+      #             ];
+      #           }
+      #         ];
+      #       };
+      #     };
+      #   };
+      # };
     };
     chromium = {
       enable = true; # Web browser
