58e35bb7e3776ee50964039d62c16f6dad51eff5
Guilhem Fauré <pro@gfaure.eu>
2025-08-30 00:48:14 +0200

    feat: config firefox

--- home/module/media/firefox/default.nix
+++ home/module/media/firefox/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ lib, config, ... }:
 {
   programs.firefox = {
     enable = true; # Web browser
@@ -8,36 +8,153 @@
       "en-US"
       "es-ES"
     ];
-    # policies = { # TODO See https://mozilla.github.io/policy-templates
-    #   BlockAboutConfig = true;
-    #   DefaultDownloadDirectory = "\${home}/Downloads";
-    #   ExtensionSettings = {
-    #     "uBlock0@raymondhill.net" = {
-    #       default_area = "menupanel";
-    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-    #       installation_mode = "force_installed";
-    #       private_browsing = true;
-    #     };
-    #   };
-    # };
-    # TODO
+    # See https://mozilla.github.io/policy-templates
+    policies = {
+      BlockAboutConfig = true;
+      DefaultDownloadDirectory = config.home.sessionVariables.XDG_DOWNLOAD_DIR;
+      ExtensionSettings = {
+        uBlockOrigin = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+        protonPass = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        darkReader = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        languageTool = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/languagetool/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        clearUrls = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        privacyBadger = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+      };
+    };
     profiles.default = {
       id = 0;
       isDefault = true;
-      # containers = {
-      #   dangerous = {
-      #     color = "red";
-      #     icon = "fruit";
-      #     id = 2;
-      #   };
-      #   shopping = {
-      #     color = "blue";
-      #     icon = "cart";
-      #     id = 1;
-      #   };
-      # };
-      # containersForce = true; # WARN force overrides containers config
-      # TODO if better than with policies
+      containers = {
+        perso = {
+          id = 0;
+          color = "blue";
+          icon = "chill";
+        };
+        tech = {
+          id = 1;
+          color = "purple";
+          icon = "briefcase";
+        };
+        music = {
+          id = 2;
+          color = "yellow";
+          icon = "circle";
+        };
+        bank = {
+          color = "green";
+          icon = "dollar";
+          id = 3;
+        };
+        shop = {
+          color = "pink";
+          icon = "cart";
+          id = 4;
+        };
+      };
+      containersForce = true; # WARN force overrides containers config
+      search.force = true; # WARN override stateful config
+      search.default = "ecosia";
+      search.privateDefault = "ecosia";
+      search.engines = {
+        ecosia = {
+          urls = [
+            {
+              template = "https://www.ecosia.org/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@eco" ];
+        };
+        nixpackages = {
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@pkg" ];
+          # icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+        };
+        nixoptions = {
+          urls = [
+            {
+              template = "https://search.nixos.org/options";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@opt" ];
+        };
+        nixwiki = {
+          urls = [
+            {
+              template = "https://wiki.nixos.org/w/index.php";
+              params = [
+                {
+                  name = "search";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nw" ];
+          # iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+        };
+        bing.metadata.hidden = true;
+      };
+      search.order = [
+        "Bookmarks"
+        "History"
+        "Actions"
+        "ecosia"
+        "Wikipedia"
+        # "YouTube" # TODO
+        "nixpackages"
+        "nixoptions"
+        "nixwiki"
+        # "Arch Wiki" # TODO
+      ];
+      userChrome = lib.readFile ./userChrome.css;
+      userContent = lib.readFile ./userContent.css;
+      # TEST if better than with policies
       # extensions = {
       #   force = true; # WARN overrides stateful settings
       #   packages = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -58,62 +175,7 @@
       #     permissions = [ "activeTab" ];
       #   };
       # };
-      # TODO Bookmarks, History, Actions, Ecosia, Wikipedia, YouTube, ArchWiki
-      # search.force = true; # WARN override stateful config
-      # search.default = "ecosia";
-      # search.privateDefault = "ecosia";
-      # search.engines = {
-      #   "Nix Packages" = {
-      #     urls = [
-      #       {
-      #         template = "https://search.nixos.org/packages";
-      #         params = [
-      #           {
-      #             name = "query";
-      #             value = "{searchTerms}";
-      #           }
-      #         ];
-      #       }
-      #     ];
-      #     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      #     definedAliases = [ "@pkg" ];
-      #   };
-      #   "Nix Options" = {
-      #     urls = [
-      #       {
-      #         template = "https://search.nixos.org/options";
-      #         params = [
-      #           {
-      #             name = "query";
-      #             value = "{searchTerms}";
-      #           }
-      #         ];
-      #       }
-      #     ];
-      #     definedAliases = [ "@opt" ];
-      #     icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      #   };
-      #   "NixOS Wiki" = {
-      #     urls = [
-      #       { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }
-      #     ];
-      #     iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
-      #     definedAliases = [ "@nw" ];
-      #   };
-      # };
-      # search.order = [
-      #   "bookmarks"
-      #   "history"
-      #   "actions"
-      #   "ecosia"
-      #   "wikipedia"
-      #   "youtube"
-      #   "Nix Packages"
-      #   "Nix Options"
-      #   "Nix Wiki"
-      #   "Arch Wiki"
-      # ];
-      # TODO if useful
+      # TEST if useful
       # settings = {
       #   "browser.startup.homepage" = "";
       #   "browser.search.region" = "GB";
@@ -128,8 +190,6 @@
       #     }
       #   ];
       # };
-      userChrome = lib.readFile ./userChrome.css;
-      userContent = lib.readFile ./userContent.css;
     };
   };
 }
