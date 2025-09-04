ccc8d4d71cd0bc17d3e5499bdc6b8fb159de7bc7
Guilhem Fauré <pro@gfaure.eu>
2025-09-04 16:52:35 +0200

    fix: allow only wheel to nix

--- system/default.nix
+++ system/default.nix
@@ -37,7 +37,7 @@
       connect-timeout = 5; # Quickly go offline if substituters not reachable
       # log-lines = lib.mkDefault 25; # More logging
     };
-    allowedUsers = [ "@wheel" ];
+    allowed-users = [ "@wheel" ];
   };
 
   boot = {
