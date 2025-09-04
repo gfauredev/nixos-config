e048f3f4f3411ad5134e508ecfb359e5d44acd11
Guilhem Fauré <pro@gfaure.eu>
2025-09-04 09:05:22 +0200

    fix: increase CPU max perf on BAT

--- system/laptop/default.nix
+++ system/laptop/default.nix
@@ -37,7 +37,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 25;
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
