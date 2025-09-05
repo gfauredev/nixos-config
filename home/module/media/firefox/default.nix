5b10516d3a497958c89b411b381c753ec66fd30e
Guilhem Fauré <pro@gfaure.eu>
2025-09-05 12:39:35 +0200

    feat(firefox): add the 38 search engines from Albert

--- home/module/media/firefox/default.nix
+++ home/module/media/firefox/default.nix
@@ -94,6 +94,150 @@
       search.default = "ecosia";
       search.privateDefault = "ecosia";
       search.engines = {
+        alternativeto = {
+          urls = [
+            {
+              template = "https://alternativeto.net/browse/search/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@alt" ];
+        };
+        amazon = {
+          urls = [
+            {
+              template = "https://www.amazon.fr/s/";
+              params = [
+                {
+                  name = "field-keywords";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@am" ];
+        };
+        archWiki = {
+          urls = [
+            {
+              template = "https://wiki.archlinux.org/index.php?title=Special:Search";
+              params = [
+                {
+                  name = "search";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@archwiki" ];
+        };
+        cdiscount = {
+          urls = [
+            {
+              template = "https://www.cdiscount.com/search/10/{searchTerms}";
+            }
+          ];
+          definedAliases = [ "@cd" ];
+        };
+        chatGPT = {
+          urls = [
+            {
+              template = "https://chat.openai.com/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@gpt" ];
+        };
+        datasheetcatalog = {
+          urls = [
+            {
+              template = "https://search.datasheetcatalog.net/key/";
+              params = [
+                {
+                  name = "key";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@dsc" ];
+        };
+        deeplEnFr = {
+          urls = [
+            {
+              template = "https://www.deepl.com/translator#en/fr/{searchTerms}";
+            }
+          ];
+          definedAliases = [ "@en-fr" ];
+        };
+        deeplFrEn = {
+          urls = [
+            {
+              template = "https://www.deepl.com/translator#fr/en/{searchTerms}";
+            }
+          ];
+          definedAliases = [ "@fr-en" ];
+        };
+        deeplFrEs = {
+          urls = [
+            {
+              template = "https://www.deepl.com/translator#fr/es/{searchTerms}";
+            }
+          ];
+          definedAliases = [ "@fr-es" ];
+        };
+        deepseek = {
+          urls = [
+            {
+              template = "https://chat.deepseek.com/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@ds" ];
+        };
+        duckDuckGo = {
+          urls = [
+            {
+              template = "https://duckduckgo.com/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@dd" ];
+        };
+        ebay = {
+          urls = [
+            {
+              template = "https://www.ebay.fr/sch/i.html";
+              params = [
+                {
+                  name = "_nkw";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@eb" ];
+        };
         ecosia = {
           urls = [
             {
@@ -106,23 +250,143 @@
               ];
             }
           ];
-          definedAliases = [ "@eco" ];
+          definedAliases = [ "@e" ];
         };
-        nixpackages = {
+        fDroid = {
           urls = [
             {
-              template = "https://search.nixos.org/packages";
+              template = "https://search.f-droid.org/";
               params = [
                 {
-                  name = "query";
+                  name = "q";
                   value = "{searchTerms}";
                 }
               ];
             }
           ];
-          definedAliases = [ "@pkg" ];
+          definedAliases = [ "@fdroid" ];
         };
-        nixoptions = {
+        gitHub = {
+          urls = [
+            {
+              template = "https://github.com/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@gh" ];
+        };
+        gitLab = {
+          urls = [
+            {
+              template = "https://gitlab.com/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@gl" ];
+        };
+        google = {
+          urls = [
+            {
+              template = "https://www.google.com/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@g" ];
+        };
+        googleMaps = {
+          urls = [
+            {
+              template = "https://www.google.com/maps/search/{searchTerms}/";
+            }
+          ];
+          definedAliases = [ "@gm" ];
+        };
+        googleScholar = {
+          urls = [
+            {
+              template = "https://scholar.google.com/scholar";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@gs" ];
+        };
+        googleTranslate = {
+          urls = [
+            {
+              template = "https://translate.google.com/";
+              params = [
+                {
+                  name = "text";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@gt" ];
+        };
+        helixDocs = {
+          urls = [
+            {
+              template = "https://docs.helix-editor.com/";
+              params = [
+                {
+                  name = "search";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@hx" ];
+        };
+        mdn = {
+          urls = [
+            {
+              template = "https://developer.mozilla.org/fr/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@mdn" ];
+        };
+        mistral = {
+          urls = [
+            {
+              template = "https://chat.mistral.ai/chat/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@mist" ];
+        };
+        nixOptions = {
           urls = [
             {
               template = "https://search.nixos.org/options";
@@ -136,7 +400,7 @@
           ];
           definedAliases = [ "@opt" ];
         };
-        nixwiki = {
+        nixWiki = {
           urls = [
             {
               template = "https://wiki.nixos.org/w/index.php";
@@ -150,19 +414,197 @@
           ];
           definedAliases = [ "@nw" ];
         };
+        nixpkgsIssues = {
+          urls = [
+            {
+              template = "https://github.com/NixOS/nixpkgs/issues";
+              params = [
+                {
+                  name = "q";
+                  value = "is:issue+is:open+{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nissue" ];
+        };
+        nixPackages = {
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@pkg" ];
+        };
+        nixVersions = {
+          urls = [
+            {
+              template = "https://lazamar.co.uk/nix-versions/";
+              params = [
+                {
+                  name = "package";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@pkgv" ];
+        };
+        nixPrTracker = {
+          urls = [
+            {
+              template = "https://nixpk.gs/pr-tracker.html";
+              params = [
+                {
+                  name = "pr";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nixpr" ];
+        };
+        noogle = {
+          urls = [
+            {
+              template = "https://noogle.dev/q";
+              params = [
+                {
+                  name = "term";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@noo" ];
+        };
+        nosToday = {
+          urls = [
+            {
+              template = "https://nos.today/search";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nos" ];
+        };
+        nostrBand = {
+          urls = [
+            {
+              template = "https://nostr.band/";
+              params = [
+                {
+                  name = "q";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nostr" ];
+        };
+        openStreetMaps = {
+          urls = [
+            {
+              template = "https://www.openstreetmap.org/search";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@m" ];
+        };
+        searchix = {
+          urls = [
+            {
+              template = "https://searchix.alanpearce.eu/";
+              params = [
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@nix" ];
+        };
+        typstDocs = {
+          urls = [
+            {
+              template = "https://typst.app/docs/{searchTerms}";
+            }
+          ];
+          definedAliases = [ "@typ" ];
+        };
+        wikipediaFr = {
+          urls = [
+            {
+              template = "https://fr.wikipedia.org/w/index.php";
+              params = [
+                {
+                  name = "search";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@wf" ];
+        };
+        wolframAlpha = {
+          urls = [
+            {
+              template = "https://www.wolframalpha.com/input/";
+              params = [
+                {
+                  name = "i";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@wa" ];
+        };
+        youtube = {
+          urls = [
+            {
+              template = "https://www.youtube.com/results";
+              params = [
+                {
+                  name = "search_query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          definedAliases = [ "@y" ];
+        };
         bing.metadata.hidden = true;
       };
       search.order = [
-        "Bookmarks"
-        "History"
-        "Actions"
-        "ecosia"
-        "Wikipedia"
-        # "YouTube" # TODO
-        "nixpackages"
-        "nixoptions"
-        "nixwiki"
-        # "Arch Wiki" # TODO
+        "Bookmarks" # Integrated into Firefox
+        "Actions" # Integrated into Firefox
+        "History" # Integrated into Firefox
+        "ecosia" # Default search engine
+        "wikipediaFr"
+        "youtube"
+        "nixWiki"
+        "archWiki"
+        "duckDuckGo"
+        "openStreetMaps"
+        "nixPackages"
+        "nixOptions"
       ];
       settings = {
         "browser.uiCustomization.navBarWhenVerticalTabs" = [
