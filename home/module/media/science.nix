d4756befc32db314e79917de8fab09796324c817
Guilhem Fauré <pro@gfaure.eu>
2025-09-11 10:42:34 +0200

    feat: quartus prime

--- home/module/media/science.nix
+++ home/module/media/science.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
+  # TODO reorganise "science"
   home.packages = with pkgs; [
     horizon-eda # Modern EDA to develop printed circuit boards
+    # quartus-prime-lite # Intel FPGA design software
     # kicad # Electronics (EDA) software BUG building
     # librepcb # Modern EDA software
     # fritzing # Elecronics (EDA) software
