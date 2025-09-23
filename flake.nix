b4ea7c00bdf46c6c062e5612a3c864c0dd53f9fc
Guilhem Fauré <pro@gfaure.eu>
2025-09-24 00:57:49 +0200

    fix: live hostplatform

--- flake.nix
+++ flake.nix
@@ -73,6 +73,7 @@
         live = lib.nixosSystem {
           # inherit system;
           modules = [
+            { nixpkgs.hostPlatform = "x86_64-linux"; }
             "${stable}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
             ./system/live.nix
           ];
