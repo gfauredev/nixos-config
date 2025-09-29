8affb6fd9fc17471d0720acb797d937babaa05a9
Guilhem Fauré <pro@guilhemfau.re>
2025-09-29 14:20:05 +0200

    fix: improve fprint enroll script

--- home/module/shell/script/fingerprints-enroll.sh
+++ home/module/shell/script/fingerprints-enroll.sh
@@ -1,5 +1,5 @@
 for finger in left-middle-finger left-index-finger right-thumb right-index-finger right-middle-finger; do
   echo "PREPARE YOUR $finger, ENROLL IS COMING …"
-  sleep 4
-  sudo fprintd-enroll -f "$finger" gf
+  sleep 3
+  sudo fprintd-enroll -f "$finger" "$USER"
 done
