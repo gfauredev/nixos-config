2299980706af946626251ab446c969f20167baab
Guilhem Fauré <pro@guilhemfau.re>
2025-10-09 19:45:50 +0200

    fix: dev uses local flake

--- home/module/shell/script/init-dev-env.sh
+++ home/module/shell/script/init-dev-env.sh
@@ -1,3 +1,4 @@
 # TODO make templates repo URL a Nix option TODO consider prioritizing local dir
-nix flake init -t "github:gfauredev/dev-templates#$1"
+# nix flake init -t "github:gfauredev/dev-templates#$1"
+nix flake init -t "$HOME/code/dev-templates#$1"
 direnv allow
