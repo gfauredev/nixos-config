3e805a9202dc41d357b96c29678949c9a1db3e90
Guilhem Fauré <pro@guilhemfau.re>
2025-10-18 22:20:20 +0200

    fix: firefox urlbar

--- home/module/web/default.nix
+++ home/module/web/default.nix
@@ -155,7 +155,7 @@
         userChrome = ''
           #navigator-toolbox:not(:hover) {
             #nav-bar:not([customizing]),
-            #urlbar:not([breakout]):not([breakout-extend]):not([focused]):not([open]),
+            #urlbar:not([breakout-extend]):not([focused]):not([open]),
             #PersonalToolbar {
               opacity: 0 !important;
               height: 0 !important;
