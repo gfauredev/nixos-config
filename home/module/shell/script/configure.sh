f4e2a110cb3ab5472ecbb79c4368aa2881f7c92c
Guilhem Fauré <pro@guilhemfau.re>
2025-10-18 17:31:08 +0200

    fix(cfg): systemd-inhibit

--- home/module/shell/script/configure.sh
+++ home/module/shell/script/configure.sh
@@ -5,11 +5,10 @@
 #   into the writeable /mnt/config and falls back to copying the
 #   readonly /etc/config-flake into /mnt/config after a 3s timeout
 # TODO make this script a package available in this flake’s nix dev environment
-
 CPU_LIMIT='cpulimit -l 888'     # Limit CPU usage to 888% accross threads
 MEM_LIMIT=$((10 * 1024 * 1024)) # Limit memory usage to 10 Go
-NIXOS_REBUILD_CMD="systemd-inhibit --what=shutdown:sleep --who=cfg --why='Reconfiguring NixOS' sudo $CPU_LIMIT nixos-rebuild"
-HOME_MANAGER_CMD="systemd-inhibit --what=shutdown:sleep --who=cfg --why='Reconfiguring Home Manager' $CPU_LIMIT home-manager"
+NIXOS_REBUILD_CMD="systemd-inhibit --what=shutdown:sleep --who=$0 --why=NixOS sudo $CPU_LIMIT nixos-rebuild"
+HOME_MANAGER_CMD="systemd-inhibit --what=shutdown:sleep --who=$0 --why=HomeManager $CPU_LIMIT home-manager"
 
 # Global CONSTANTS
 SUBFLAKE='public' # Sub flake (usually public config) location
