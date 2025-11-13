9c04066798a8f9d663c30400a31f4eca8cd50b76
Guilhem Fauré <pro@guilhemfau.re>
2025-11-13 19:38:08 +0100

    feat: lisp tooling

--- home/griffin.nix
+++ home/griffin.nix
@@ -30,8 +30,7 @@
     sherlock # Search people on social media accounts
     # wayback # Web archiving tool
     qgis # Maps TODO https://www.mypermagarden.app/fr/download
-    sbcl # Common Lisp compiler and runtime
-    guile # Scheme compiler and runtime
+    # guile # Scheme compiler and runtime
     # Electronics
     # rpi-imager # Raspberry Pi OS imager
     # horizon-eda # Modern EDA to develop printed circuit boards
