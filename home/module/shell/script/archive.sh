b7f98edf01c812404173f9bc72e63998307b6905
Guilhem Fauré <pro@guilhemfau.re>
2025-11-20 14:49:58 +0100

    fix: archive

--- home/module/shell/script/archive.sh
+++ home/module/shell/script/archive.sh
@@ -1,20 +1,40 @@
-ARCHIVE=$HOME/archive
-
-source=$PWD
-if [ -n "$1" ]; then
-  source=$(realpath "$1")
-fi
-
-if [ "${source#"$HOME"}" != "$source" ]; then
-  dest="$ARCHIVE/${source#"$HOME"/}" # Remove the home directory prefix
-  printf "Archiving %s under %s\n" "$source" "$dest"
+if [ -z "$1" ]; then
+    TARGET_PATH=$(pwd) # No arg provided: Target is the current directory
 else
-  # Not under the user's home directory, print an error and exit
-  printf "Error: Not under user home directory.\n" >&2
-  exit 1
+    # Arg provided: Target is the specific file/folder
+    TARGET_PATH=$(realpath "$1")
 fi
-
-mkdir --parents --verbose "$dest"
-mv --verbose "$source"/* "$dest"
-mv --verbose "$source"/.* "$dest"
-rmdir --verbose "$source"
+if [ ! -e "$TARGET_PATH" ]; then # Ensure file exists
+    echo "Error: '$TARGET_PATH' does not exist"
+    exit 1
+fi
+if [[ "$TARGET_PATH" != "$HOME"* ]]; then # Ensure file is inside HOME
+    echo "Error: Target must be under your home directory ($HOME)"
+    exit 1
+fi
+REL_PATH_FROM_HOME="${TARGET_PATH#"$HOME"/}"
+HOME_DIRECT_CHILD="${REL_PATH_FROM_HOME%%/*}"
+if [ "$REL_PATH_FROM_HOME" == "$HOME_DIRECT_CHILD" ]; then
+    echo "Error: Cannot archive direct child of Home, must be in a subdirectory"
+    exit 1 # Prevent archiving root items
+fi
+INNER_PATH="${REL_PATH_FROM_HOME#"$HOME_DIRECT_CHILD"/}"
+ARCHIVE_ROOT="$HOME/archive-$HOME_DIRECT_CHILD"
+DEST_PATH="$ARCHIVE_ROOT/$INNER_PATH"
+DEST_DIR=$(dirname "$DEST_PATH")
+echo "Archiving: $TARGET_PATH -> $DEST_PATH"
+mkdir --parents --verbose "$DEST_DIR" # Create directory structure
+if [ "$TARGET_PATH" = "$(pwd)" ]; then
+    cd .. # Step back if we are archiving the current directory
+fi
+mv --verbose "$TARGET_PATH" "$DEST_PATH" # Archive the file/folder (move it)
+if [ $? -eq 0 ]; then
+    echo "✓ Success"
+    if [ "$IS_CWD" = true ]; then # Remind the user if inside the moved folder
+        echo "NOTE: You are currently in a deleted directory,"
+        echo "      type 'cd ..' or 'cd -' to return to a valid path"
+    fi
+else
+    echo "✗ Failed to move file"
+    exit 1
+fi
