0bdc8846e4b580679fbd347844fcab63479c75ff
Guilhem Fauré <pro@guilhemfau.re>
2025-12-17 21:29:43 +0100

    fix: don’t inhib poweroff in cfg to allow reboot

--- home/module/shell/default.nix
+++ home/module/shell/default.nix
@@ -21,7 +21,7 @@
       # FIXME allow the script itself to sleep / poweroff / reboot
       configure = writeScriptBin "cfg" ''
         cd ${config.location}
-        systemd-inhibit --what=shutdown:sleep --who=cfg --why=Configuring ${writeScript "configure" (readFile ./script/configure.sh)} "$@"
+        systemd-inhibit --what=sleep --who=cfg --why=Configuring ${writeScript "configure" (readFile ./script/configure.sh)} "$@"
       '';
       date-edit = writeScriptBin "de" (readFile ./script/date-edit.sh);
       extract = writeScriptBin "ex" (readFile ./script/extract.sh);
