4f1e0b1ffcebf685b2e5134372eb6e0039bb9fef
Guilhem Fauré <pro@guilhemfau.re>
2026-01-18 13:51:08 +0100

    udev rule

--- system/module/pc.nix
+++ system/module/pc.nix
@@ -125,6 +125,8 @@
         SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6003", MODE="0666"
         SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666"
         SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666"
+
+        SUBSYSTEMS=="usb", ATTRS{manufacturer}=="NVIDIA Corp.", ATTRS{product}=="APX", GROUP="wheel"
       '';
     };
     geoclue2 = {
