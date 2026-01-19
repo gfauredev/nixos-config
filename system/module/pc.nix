ef428c0b27f64f663f170101266fbfe4ed65e9d6
Guilhem Fauré <pro@guilhemfau.re>
2026-01-19 23:17:39 +0100

    udev

--- system/module/pc.nix
+++ system/module/pc.nix
@@ -126,7 +126,9 @@
         SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666"
         SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666"
 
-        SUBSYSTEMS=="usb", ATTRS{manufacturer}=="NVIDIA Corp.", ATTRS{product}=="APX", GROUP="wheel"
+        SUBSYSTEM=="usb", ATTRS{manufacturer}=="NVIDIA Corp.", ATTRS{product}=="APX", GROUP="wheel"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", MODE="0666"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="0955", ATTRS{idProduct}=="7321", MODE="0666"
       '';
     };
     geoclue2 = {
