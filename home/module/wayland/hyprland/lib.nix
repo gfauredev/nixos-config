4f8848da2c857f93ff865f8f803f782d11e6303b
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 10:51:06 +0100

    fix: hyprland conf

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -3,18 +3,20 @@
   genBinds =
     workspaceSet:
     let
-      notEmpty = ws: ''hyprctl clients -j | jq -e "any(.[]; .workspace.name == '${ws}')"'';
-      focused = ws: ''[ $(hyprctl activeworkspace -j | jq -r '.name') = "${ws}" ]'';
+      notEmpty = ws: "hyprctl clients -j | jq -e 'any(.[]; .workspace.name == '${ws}')'";
+      focused = ws: "[ $(hyprctl activeworkspace -j | jq -r '.name') = '${ws}' ]";
       mod = "SUPER"; # Main modifier, SUPER
-      # Logic for $mod + Key TODO Case where not empty + already focused
-      focusCmd = ws: "${notEmpty ws.name} || ${ws.empty or config.launch.app}";
+      # Logic for $mod + Key
+      alreadyFocusedCmd = ws: "${focused ws.name} && ${ws.empty or config.launch.app}";
+      notEmptyCmd = ws: "${notEmpty ws.name} || ${ws.empty or config.launch.app}";
       # Logic for $mod + SHIFT + Key
       shiftCmd = ws: "${focused ws.name} && hyprctl dispatch movecurrentworkspacetomonitor +1";
     in
     lib.concatLists (
       lib.mapAttrsToList (key: ws: [
+        "${mod}, ${key}, exec, ${alreadyFocusedCmd ws}"
         "${mod}, ${key}, workspace, name:${ws.name}"
-        "${mod}, ${key}, exec, ${focusCmd ws}"
+        "${mod}, ${key}, exec, ${notEmptyCmd ws}"
         "${mod} SHIFT, ${key}, exec, ${shiftCmd ws}"
         "${mod} SHIFT, ${key}, movetoworkspace, name:${ws.name}"
       ]) workspaceSet
