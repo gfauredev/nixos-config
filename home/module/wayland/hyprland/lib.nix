a901f48d023811af1ae0de7813fdd23141195092
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 15:00:51 +0100

    fix: hyprland win rules

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -7,16 +7,15 @@
       windowCount =
         ws: "hyprctl workspaces -j | jq -r '.[] | select(.name == \"${ws.name}\") | .windows // 0'";
       ifFocus = ws: cmd: "[ $(hyprctl activeworkspace -j | jq -r '.name') = '${ws.name}' ] && ${cmd}";
-      windowRules = ws: ":;"; # TODO
       # Logic for $mod + Key
-      ifEmpty = ws: cmd: "[ $(${windowCount ws}) -eq 0 ] && ${cmd}";
       workspace = name: "hyprctl dispatch workspace name:${name}";
+      ifEmpty = ws: cmd: "[ $(${windowCount ws}) -eq 0 ] && ${cmd}";
       # Logic for $mod + SHIFT + Key
       shift = ws: ifFocus ws "hyprctl dispatch movecurrentworkspacetomonitor +1";
     in
     lib.concatLists (
       lib.mapAttrsToList (key: ws: [
-        "${mod}, ${key}, exec, ${windowRules ws} ${
+        "${mod}, ${key}, exec, [${ws.alreadyWinRules or ""}] ${
           ifFocus ws (ws.already or config.launch.app)
         } || ${workspace ws.name} && ${ifEmpty ws (ws.empty or config.launch.app)}"
         "${mod} SHIFT, ${key}, exec, ${shift ws}"
--- home/module/wayland/hyprland/workspaces.nix
+++ home/module/wayland/hyprland/workspaces.nix
@@ -14,7 +14,8 @@ rec {
   a = {
     name = "art"; # Artistic creation
     empty = "${config.launch.category} AudioVideo"; # Exec if focused + empty
-    already = "[float; center; size 888 420] ${config.launch.mix}"; # Already focusing ws
+    alreadyWinRules = "float; center; size 888 420"; # TODO Cleaner
+    already = config.launch.mix; # Already focusing ws
     icon = ""; # TODO Use it in waybar identified by name
   };
   b = {
