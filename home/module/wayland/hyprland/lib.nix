d3f1ea8d6ff38698282c795febc8617921d50bf8
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 15:17:52 +0100

    fix: hyprland win rules

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -6,20 +6,18 @@
       mod = "SUPER"; # Main modifier, SUPER
       windowCount =
         ws: "hyprctl workspaces -j | jq -r '.[] | select(.name == \"${ws.name}\") | .windows // 0'";
-      ifFocus =
-        ws:
-        "[ $(hyprctl activeworkspace -j | jq -r '.name') = '${ws.name}' ] && ${
-          ws.already or config.launch.app
-        }";
+      ifFocus = ws: cmd: "[ $(hyprctl activeworkspace -j | jq -r '.name') = '${ws.name}' ] && ${cmd}";
       # Logic for $mod + Key
       workspace = ws: "hyprctl dispatch workspace name:${ws.name}";
-      ifEmpty = ws: "[ $(${windowCount ws}) -eq 0 ] && ${ws.empty or config.launch.app}";
+      empty = ws: "[ $(${windowCount ws}) -eq 0 ] && ${ws.empty or config.launch.app}";
       # Logic for $mod + SHIFT + Key
       shift = ws: ifFocus ws "hyprctl dispatch movecurrentworkspacetomonitor +1";
     in
     lib.concatLists (
       lib.mapAttrsToList (key: ws: [
-        "${mod}, ${key}, exec, ${ws.alreadyWinRules or ":;"} ${ifFocus ws} || ${workspace ws} && ${ifEmpty ws}"
+        "${mod}, ${key}, exec, ${ws.alreadyWinRules or ":;"} ${
+          ifFocus ws (ws.already or config.launch.app)
+        } || ${workspace ws} && ${empty ws}"
         "${mod} SHIFT, ${key}, execr, ${shift ws}"
         "${mod} SHIFT, ${key}, movetoworkspace, name:${ws.name}"
       ]) workspaceSet
