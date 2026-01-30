b817f933e55431d1d54e400f719ce060ff5e6200
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 01:42:15 +0100

    fix: hyprland declarative ws

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -7,22 +7,18 @@
       focused = ws: ''[ $(hyprctl activeworkspace -j | jq -r '.name') = "${ws}" ] &&'';
       mod = "SUPER"; # Main modifier, SUPER
       # Logic for $mod + Key
-      focusCmd = ws: ''
-        ${empty ws.name} "${ws.empty or config.launch.app}" || ${focused ws.name} "${
-          ws.already or config.launch.app
-        }"
-      '';
+      focusCmd = ws: "${empty ws.name} ${ws.empty or config.launch.app}";
       # Logic for $mod + SHIFT + Key
-      shiftCmd = ws: ''
-        ${focused ws.name} hyprctl dispatch movecurrentworkspacetomonitor +1
-      '';
+      # shiftCmd = ws: ''
+      #   ${focused ws.name} hyprctl dispatch movecurrentworkspacetomonitor +1
+      # '';
     in
     lib.concatLists (
       lib.mapAttrsToList (key: ws: [
         "${mod}, ${key}, workspace, name:${ws.name}"
         "${mod}, ${key}, exec, ${focusCmd ws}"
         "${mod} SHIFT, ${key}, movetoworkspace, name:${ws.name}"
-        "${mod} SHIFT, ${key}, exec, ${shiftCmd ws}"
+        # "${mod} SHIFT, ${key}, exec, ${shiftCmd ws}"
       ]) workspaceSet
     );
 }
