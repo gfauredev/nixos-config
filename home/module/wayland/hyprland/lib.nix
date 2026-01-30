57c804ff2f35645e5f5508593ac6854338adead0
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 12:59:26 +0100

    fix: tmp hyprland ws

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -17,13 +17,15 @@
           exec = "hyprctl dispatch exec";
           workspace = "hyprctl dispatch workspace";
         in
+        # FIXME Works in shell but not in Hyprland’s exec
         "${focused ws.name} ${exec} '${cmdAlready}' || ${workspace} name:${ws.name} && ${empty ws.name} ${exec} '${cmdEmpty}'";
       # Logic for $mod + SHIFT + Key
       shiftCmd = ws: "${focused ws.name} && hyprctl dispatch movecurrentworkspacetomonitor +1";
     in
     lib.concatLists (
       lib.mapAttrsToList (key: ws: [
-        "${mod}, ${key}, exec, ${toggleCmd ws}"
+        # "${mod}, ${key}, exec, ${toggleCmd ws}" FIXME
+        "${mod}, ${key}, workspace, ${ws.name}" # Temporary FIX
         "${mod} SHIFT, ${key}, exec, ${shiftCmd ws}"
         "${mod} SHIFT, ${key}, movetoworkspace, name:${ws.name}"
       ]) workspaceSet
