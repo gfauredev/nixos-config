1d59650a50d62109677adf75ebdbcc726e13a8e8
Guilhem Fauré <pro@guilhemfau.re>
2026-01-30 15:20:32 +0100

    fix: hyprland win rules

--- home/module/wayland/hyprland/lib.nix
+++ home/module/wayland/hyprland/lib.nix
@@ -8,7 +8,7 @@
         ws: "hyprctl workspaces -j | jq -r '.[] | select(.name == \"${ws.name}\") | .windows // 0'";
       ifFocus = ws: cmd: "[ $(hyprctl activeworkspace -j | jq -r '.name') = '${ws.name}' ] && ${cmd}";
       # Logic for $mod + Key
-      workspace = ws: "hyprctl dispatch workspace name:${ws.name}";
+      workspace = ws: "hyprctl dispatch workspace name:${ws.name} && sleep 0.05";
       empty = ws: "[ $(${windowCount ws}) -eq 0 ] && ${ws.empty or config.launch.app}";
       # Logic for $mod + SHIFT + Key
       shift = ws: ifFocus ws "hyprctl dispatch movecurrentworkspacetomonitor +1";
