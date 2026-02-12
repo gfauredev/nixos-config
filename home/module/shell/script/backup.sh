d0634be7ffabccad68ca3690e59586a75418ff10
Guilhem Fauré <pro@guilhemfau.re>
2026-02-12 20:38:38 +0100

    fix: restic pack size

--- home/module/shell/script/backup.sh
+++ home/module/shell/script/backup.sh
@@ -19,62 +19,69 @@ _restic() { # Custom restic command
   shift
   systemd-inhibit --what=shutdown:sleep --who="$0" --why=Backuping \
   restic --repo "$REPO" --verbose backup --exclude-caches \
-  --exclude-file="$XDG_CONFIG_HOME"/backup-exclude/common "$@" # TODO Nix deriv
+  --exclude-file="$XDG_CONFIG_HOME"/backup-exclude/common "$@" \
+  --pack-size=128 # Less chunks for drives # TODO Nix deriv
 }
 
-if [ "$avail" -gt "$used" ]; then
-  case "$1" in
-  *drive*)
-    # Backup everything in remote drive with rclone, putting deleted aside
-    for dir in $IMPORTANT; do
-      dirname=$(basename "$dir")
-      rclone sync --progress \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/common \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
-      --backup-dir "$1:$USER-trash/$(date +%Y-%m-%d-%H)/$dirname" \
-      "$dir" "$1:$USER-back/$dirname" 
-    done
-    for dir in $ARCHIVE; do
-      dirname=$(basename "$dir")
-      rclone copy --progress \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/common \
-      --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
-      "$dir" "$1:$USER-back/archive/$dirname" 
-    done
-    ;;
-  *back*)
+case "$1" in
+*drive*)
+  # Backup everything in remote drive with rclone, putting deleted aside
+  for dir in $IMPORTANT; do
+    dirname=$(basename "$dir")
+    rclone sync --progress \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/common \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
+    --backup-dir "$1:$USER-trash/$(date +%Y-%m-%d-%H)/$dirname" \
+    "$dir" "$1:$USER-back/$dirname" 
+  done
+  for dir in $ARCHIVE; do
+    dirname=$(basename "$dir")
+    rclone copy --progress \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/common \
+    --exclude-from="$XDG_CONFIG_HOME"/backup-exclude/img \
+    "$dir" "$1:$USER-back/archive/$dirname" 
+  done
+  ;;
+*back*)
+  if [ "$avail" -gt "$used" ]; then
     # Backup everything incrementally with restic in backup drives
     # (which label contains "back")
     printf "%s contains back: " "$1"
     printf "Backing up [%s %s] incrementally (restic)\n" "$IMPORTANT" "$ARCHIVE"
     _restic "$1" $IMPORTANT $ARCHIVE
-      
-    ;;
-  *boot*)
-    # Store most important directories and OSes in large drives or sticks
-    # (which label don’t contains "back" but contains "boot")
-    printf "%s contains boot: " "$1"
-    printf "Backing up [%s] encrypted, as well as [%s]\n" "$IMPORTANT" "$BOOTABLE"
-    printf "\nTODO with a crossplatform encrypted archiver (7zip, Veracrypt…)\n"
-    # _rsync --delete $BOOTABLE/ "$1" # Sync bootable files at root of the drive
-    _rsync $IMPORTANT "$1" # Sync important dirs at the root of the drive
-    ;;
-  *)
-    # Store only most important directories in large drives or sticks
-    # (which label don’t contains "back" nor "boot")
-    printf "%s don’t contains back: " "$1"
+    while [ "$#" -gt 0 ]; do
+      case "$2" in
+      *drive*) # Sync restic dir to cloud provider TODO periodically with Nix
+        rclone sync --progress "$1" "$2:$USER-restic" &
+      esac
+      shift # Next argument
+    done
+  else
+    # Don’t store large dirs/files in too small drives
+    printf "%s doesn’t have enough available space: " "$1"
     printf "Backing up [%s] in encrypted archive\n" "$IMPORTANT"
     printf "\nTODO with a crossplatform encrypted archiver (7zip, Veracrypt…)\n"
-    _rsync $IMPORTANT "$1" # Sync important dirs at the root of the drive
-    ;;
-  esac
-else
-  # Don’t store large dirs/files in too small drives
-  printf "%s doesn’t have enough available space: " "$1"
+    _rsync --exclude="*.large" $IMPORTANT "$1" # Sync important dirs
+  fi
+  ;;
+*boot*)
+  # Store most important directories and OSes in large drives or sticks
+  # (which label don’t contains "back" but contains "boot")
+  printf "%s contains boot: " "$1"
+  printf "Backing up [%s] encrypted, as well as [%s]\n" "$IMPORTANT" "$BOOTABLE"
+  printf "\nTODO with a crossplatform encrypted archiver (7zip, Veracrypt…)\n"
+  # _rsync --delete $BOOTABLE/ "$1" # Sync bootable files at root of the drive
+  _rsync $IMPORTANT "$1" # Sync important dirs at the root of the drive
+  ;;
+*)
+  # Store only most important directories in large drives or sticks
+  # (which label don’t contains "back" nor "boot")
+  printf "%s don’t contains back: " "$1"
   printf "Backing up [%s] in encrypted archive\n" "$IMPORTANT"
   printf "\nTODO with a crossplatform encrypted archiver (7zip, Veracrypt…)\n"
-  _rsync --exclude="*.large" $IMPORTANT "$1" # Sync important dirs
-fi
+  _rsync $IMPORTANT "$1" # Sync important dirs at the root of the drive
+  ;;
+esac
 
 echo -n "Clean archive directories content ? (y to accept): "
 read -r shouldClean # TODO Timeout after 5s in POSIX shell
