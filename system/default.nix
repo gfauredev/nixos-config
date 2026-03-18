02493997e979aa43b46d4529518d40160a45e4d5
Guilhem Fauré <pro@guilhemfau.re>
2026-03-18 20:07:29 +0100

    fix: substituters

--- system/default.nix
+++ system/default.nix
@@ -5,14 +5,15 @@
       "nix-command"
       "flakes"
     ];
-    extra-substituters = [
+    substituters = [
+      "https://cache.nixos.org"
       "https://cache.garnix.io"
-      # "https://nix-community.cachix.org"
+      # "https://hydra.nixos.org"
     ];
-    extra-trusted-public-keys = [
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       # "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     max-jobs = lib.mkDefault 8; # Save some threads for rest of system
     cores = lib.mkDefault 8; # Save some threads for rest of system
