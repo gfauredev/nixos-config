03f9ecc02e8acd17a3a27a1c9cda01aaab43aa61
Guilhem Fauré <pro@guilhemfau.re>
2026-03-21 02:06:55 +0100

    fix: gemini container

--- home/module/gemini/env.sh
+++ home/module/gemini/env.sh
@@ -1,9 +1,10 @@
-# NIX_REMOTE="daemon --volume /etc/nix/nix.conf:/etc/nix/nix.conf:ro"
+# NIX_REMOTE="daemon"
 # NIX_CONFIG="accept-flake-config = true"
 # NO_BROWSER=true
 GEMINI_SANDBOX_IMAGE=gemini-sandbox
 SANDBOX_SET_UID_GID=true
-SANDBOX_FLAGS="--volume /nix/store:/nix/store:O --volume /nix/var/nix/daemon-socket/socket:/nix/var/nix/daemon-socket/socket:rw --volume /etc/nix/nix.conf:/etc/nix/nix.conf:ro --volume /etc/ssl/certs:/etc/ssl/certs:ro --volume /etc/static/ssl:/etc/static/ssl:ro --volume /run/current-system/sw/bin:/run/current-system/sw/bin:ro --env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/share/npm-global/bin:/run/current-system/sw/bin --env NIX_REMOTE --env NIX_CONFIG"
+SANDBOX_FLAGS="--volume /nix/store:/nix/store:O --volume /nix/var/nix/daemon-socket/socket:/nix/var/nix/daemon-socket/socket:rw --volume /etc/nix/nix.conf:/etc/nix/nix.conf:ro --volume /etc/ssl/certs:/etc/ssl/certs:ro --volume /etc/static/ssl:/etc/static/ssl:ro --volume /run/current-system/sw/bin:/run/current-system/sw/bin:ro --env PATH --env NIX_REMOTE --env NIX_CONFIG"
+# --env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/share/npm-global/bin:/run/current-system/sw/bin
 # --security-opt label=disable
 # --volume /nix:/nix:O
 # --volume /nix/var/nix/db:/nix/var/nix/db:O
