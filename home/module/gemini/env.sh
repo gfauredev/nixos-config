27a5761dccffc72726af72207e12d7bb4f2935a4
Guilhem Fauré <pro@guilhemfau.re>
2026-03-22 16:44:46 +0100

    fix: gemini home

--- home/module/gemini/env.sh
+++ home/module/gemini/env.sh
@@ -3,25 +3,21 @@
 # NO_BROWSER=true
 GEMINI_SANDBOX_IMAGE=gemini-sandbox
 SANDBOX_SET_UID_GID=true
-SANDBOX_FLAGS="--volume /nix/store:/nix/store:O --volume /nix/var/nix/daemon-socket/socket:/nix/var/nix/daemon-socket/socket:rw --volume /etc/nix/nix.conf:/etc/nix/nix.conf:ro --volume /etc/ssl/certs:/etc/ssl/certs:ro --volume /etc/static/ssl:/etc/static/ssl:ro --volume /run/current-system/sw/bin:/run/current-system/sw/bin:ro --env HOME=/home/node --env PATH --env NIX_REMOTE --env NIX_CONFIG"
-# --env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/share/npm-global/bin:/run/current-system/sw/bin
+SANDBOX_FLAGS="--volume /nix/store:/nix/store:O --volume /nix/var/nix/daemon-socket/socket:/nix/var/nix/daemon-socket/socket:rw --volume /etc/nix/nix.conf:/etc/nix/nix.conf:ro --volume /etc/ssl/certs:/etc/ssl/certs:ro --volume /etc/static/ssl:/etc/static/ssl:ro --volume /run/current-system/sw/bin:/run/current-system/sw/bin:ro --volume /home/gf/.cargo:O --env PATH --env NIX_REMOTE --env NIX_CONFIG"
+# TODO Variabilise home dir, use it for .cargo
 # --security-opt label=disable
+# --userns=keep-id
+# --workdir /workspace
+# --tmpfs /tmp
+# --tmpfs /nix/var/nix:rw,exec,mode=1777
 # --volume /nix:/nix:O
 # --volume /nix/var/nix/db:/nix/var/nix/db:O
 # --volume /nix/var/nix/profiles:/nix/var/nix/profiles:O
 # --volume /nix/var/nix/temproots:/nix/var/nix/temproots:O
 # --volume /nix/var/nix/gcroots:/nix/var/nix/gcroots:O
-#  --tmpfs /nix/var/nix:rw,exec,mode=1777
-# --volume /nix/var/nix/daemon-socket/socket:/nix/var/nix/daemon-socket/socket:ro
 # --volume /etc/nix:/etc/nix:O
 # --volume /etc/ssl/certs:/etc/ssl/certs:ro
 # --volume /etc/static/ssl:/etc/static/ssl:ro
 # --volume /run/current-system/sw:/run/current-system/sw:ro
 # --volume $HOME/.gemini:/workspace/.gemini:rw
 # --volume $PWD:/workspace:rw
-# --userns=keep-id
-# --workdir /workspace
-# --tmpfs /tmp
-# --env HOME=/workspace
-# --env PATH=$PATH:/run/current-system/sw/bin
-# --env PATH="$(dirname $(readlink -f $(which nix)))":/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
--- home/module/shell/nushell/config.nu
+++ home/module/shell/nushell/config.nu
@@ -151,6 +151,7 @@ def agent [...args] {
         NIX_REMOTE: "daemon"
         NIX_CONFIG: ($nix_settings | str join "\n")
         PATH: ($paths | append $env.PATH)
+        HOME: "/home/node"
     } { 
         gemini --yolo ...$args 
     }
