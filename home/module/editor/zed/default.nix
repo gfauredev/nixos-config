b80a0cfb046992d17546872b6b56b127ac028293
Guilhem Fauré <pro@guilhemfau.re>
2026-03-28 21:45:20 +0100

    feat: zed

--- home/module/editor/zed/default.nix
+++ home/module/editor/zed/default.nix
@@ -1,6 +1,53 @@
-{ pkgs, ... }:
+{ config, pkgs-unstable, ... }:
 {
-  home.packages = with pkgs; [
-    zed-editor # Modern text editor
-  ];
+  programs.zed-editor.enable = true;
+
+  programs.zed-editor = {
+    package = pkgs-unstable.zed-editor;
+    # defaultEditor = true;
+    extraPackages = config.editor.commonPackages;
+    extensions = [
+      # "bash"
+      # "biome-zed"
+      "dockerfile"
+      "emmet"
+      "harper"
+      # "haskell"
+      "html" # "zed/extensions/html"
+      # "html-jinja"
+      # "html-snippets"
+      # "htmx-lsp"
+      # "http"
+      # "hyprlang"
+      "java"
+      # "java-eclipse-jdtls"
+      # "javascript-snippets"
+      # "jj-lsp"
+      # "jq"
+      # "json5"
+      # "jsonl"
+      # "just"
+      "kotlin"
+      # "ltex"
+      "lua"
+      "make"
+      # "neocmake"
+      # "nginx"
+      # "nickel"
+      "nix"
+      "nu"
+      # "nu-lint"
+      # "racket"
+      # "rust-snippets"
+      # "scheme"
+      "sql"
+      # "scss"
+      "toml"
+      # "typescript-snippets"
+      # "typos"
+      "typst"
+      # "zig"
+    ];
+    installRemoteServer = true;
+  };
 }
