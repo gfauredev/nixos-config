ea2907064a513e8c3177537c4eca5af1b10df3a6
Guilhem Fauré <pro@guilhemfau.re>
2026-06-16 23:54:23 +0200

    fix: agent conf

--- home/module/agent/default.nix
+++ home/module/agent/default.nix
@@ -1,9 +1,9 @@
 { config, ... }: {
   config.home.file = {
-    "~/.gemini/antigravity-cli/settings.json".source =
+    ".gemini/antigravity-cli/settings.json".source =
       config.lib.file.mkOutOfStoreSymlink # br
         "${config.location}/public/home/module/antigravity/settings.json";
-    "~/.gemini/antigravity-cli/keybindings.json".source =
+    ".gemini/antigravity-cli/keybindings.json".source =
       config.lib.file.mkOutOfStoreSymlink # br
         "${config.location}/public/home/module/antigravity/keybindings.json";
   };
