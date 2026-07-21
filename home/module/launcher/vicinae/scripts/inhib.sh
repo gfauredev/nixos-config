af642b84fe06da87415af7bff0249aed7354f96f
Guilhem Fauré <pro@guilhemfau.re>
2026-07-21 16:17:02 +0200

    fix: inhib script

--- home/module/launcher/vicinae/scripts/inhib.sh
+++ home/module/launcher/vicinae/scripts/inhib.sh
@@ -5,8 +5,9 @@
 # @vicinae.exec ["/bin/sh"]
 # @vicinae.argument1 { "type": "text", "placeholder": "1h" }
 
-(if systemd-inhibit --why="inhib.sh, notably used by Vicinae launcher" sleep "$1"; then
+(
   notify-send "Inhibiting sleep" "For $1"
-else
-  notify-send "Failed to inhibit sleep" "$(systemd-inhibit)"
-fi) &
+  if ! systemd-inhibit --why="inhib.sh, notably used by Vicinae launcher" sleep "$1"; then
+    notify-send "Failed to inhibit sleep" "$(systemd-inhibit)"
+  fi
+) &
